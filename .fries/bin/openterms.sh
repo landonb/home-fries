1b06c4592463578a329683ce34aea98ea7c07d66
Landon Bouma <landonb@users.noreply.github.com>
2015-01-27 03:45:11 -0600

    Adaptive mate-terminal wrappers.

---
+++ .fries/bin/openterms.sh
@@ -0,0 +1,215 @@
+#!/bin/bash
+
+# File: openterms.sh
+# Author: Landon Bouma (dubsacks &#x40; retrosoft &#x2E; com)
+# Last Modified: 2015.01.24
+# Project Page: https://github.com/landonb/home_fries
+# License: GPLv3
+
+"""
+Open and Setup Lots of Terminal Windows
+=======================================
+
+This script helps you prepare your work environment
+by opening a bunch of terminal windows to friendly
+dimensions and screen placesments, and changing
+directories and running scripts as specified.
+
+This script basically calls termdub.py a bunch;
+see that script for more details.
+"""
+
+# 2012.10.17: This script created so I don't need to click so many terminal
+#             buttons... and so I don't need to keep editing Gnome shortcuts.
+
+# NOTE: To kill all existing gnome-terminals (it's just one process), run,
+#
+#   killsomething gnome-terminal
+
+# NOTE: If we don't use & to execute these terminals in the background, the
+#       gnome shortcut opens just the first terminal, and then when you close
+#       that one, it'll open the second terminal, etc.
+# NOTE: If we don't wait between each terminal being opened, they won't claim
+#       their proper place in the gnome taskbar.
+
+# Save a few lines of code to make $script_path,
+# which will be, e.g., /home/$USER/.fries/bin.
+DEBUG_TRACE=false
+source $(dirname $0)/bash_base.sh
+
+function do_sleep () {
+  echo "import time;time.sleep(0.5)" | python
+}
+
+# Make sure we reset the DUBs vars btw calls, otherwise they'll be used in
+# subsequent commands (if you're running this openterms.sh script from a
+# gnome taskbar shortcut).
+
+# 
+DUBS_TERMNAME="rpdb" \
+  DUBS_STARTIN=/ccp/dev/ccpv3_trunk/pyserver/bin/winpdb \
+  DUBS_STARTUP="/usr/bin/python rpdb2.py" \
+  $script_path/termdub.py -t dbms \
+  &
+do_sleep
+
+DUBS_TERMNAME="" \
+  DUBS_STARTIN=/ccp/bin/ccpdev \
+  DUBS_STARTUP="" \
+  $script_path/bin/termdub.py -t dbms \
+  &
+do_sleep
+
+#
+DUBS_TERMNAME="" \
+  DUBS_STARTIN="" \
+  DUBS_STARTUP="" \
+  $script_path/bin/termdub.py -t dbms \
+  &
+do_sleep
+
+DUBS_TERMNAME="" \
+  DUBS_STARTIN="" \
+  DUBS_STARTUP="" \
+  $script_path/bin/termdub.py -t dbms \
+  &
+do_sleep
+
+#
+DUBS_TERMNAME="V3" \
+  DUBS_STARTIN=/ccp/dev/cp_trunk_v3 \
+  DUBS_STARTUP="" \
+  $script_path/bin/termdub.py -t dbms \
+  &
+do_sleep
+
+DUBS_TERMNAME="Working" \
+  DUBS_STARTIN=/ccp/dev/cp/flashclient \
+  DUBS_STARTUP="" \
+  $script_path/bin/termdub.py -t dbms \
+  &
+do_sleep
+
+#
+DUBS_TERMNAME="Logs" \
+  DUBS_STARTIN=/ccp/var/log/daily \
+  DUBS_STARTUP="logs" \
+  $script_path/bin/termdub.py -t logs \
+  &
+do_sleep
+
+DUBS_TERMNAME="Logc" \
+  DUBS_STARTIN=/ccp/var/log/daily \
+  DUBS_STARTUP="logc" \
+  $script_path/bin/termdub.py -t logc \
+  &
+do_sleep
+
+#
+DUBS_TERMNAME="Psql-v3" \
+  DUBS_STARTIN="" \
+  DUBS_STARTUP="psql -U cycling ccpv3_lite" \
+  $script_path/bin/termdub.py -t logs \
+  &
+do_sleep
+
+DUBS_TERMNAME="Psql-v2" \
+  DUBS_STARTIN="" \
+  DUBS_STARTUP="psql -U cycling ccpv3_lite" \
+  $script_path/bin/termdub.py -t logc \
+  &
+do_sleep
+
+#
+DUBS_TERMNAME="rLogs" \
+  DUBS_STARTIN="" \
+  DUBS_STARTUP="sss runic" \
+  $script_path/bin/termdub.py -t logs \
+  &
+do_sleep
+
+DUBS_TERMNAME="rPsql" \
+  DUBS_STARTIN="" \
+  DUBS_STARTUP="sss runic" \
+  $script_path/bin/termdub.py -t logc \
+  &
+do_sleep
+
+#
+DUBS_TERMNAME="rLogs_Rd" \
+  DUBS_STARTIN="" \
+  DUBS_STARTUP="sss runic" \
+  $script_path/bin/termdub.py -t logs \
+  &
+do_sleep
+
+DUBS_TERMNAME="rPsql2" \
+  DUBS_STARTIN="" \
+  DUBS_STARTUP="sss runic" \
+  $script_path/bin/termdub.py -t logc \
+  &
+do_sleep
+
+#
+DUBS_TERMNAME="" \
+  DUBS_STARTIN="" \
+  DUBS_STARTUP="sss runic" \
+  $script_path/bin/termdub.py -t dbms \
+  &
+do_sleep
+
+DUBS_TERMNAME="" \
+  DUBS_STARTIN="" \
+  DUBS_STARTUP="" \
+  $script_path/bin/termdub.py -t dbms \
+  &
+do_sleep
+
+#
+DUBS_TERMNAME="" \
+  DUBS_STARTIN="" \
+  DUBS_STARTUP="sss runic" \
+  $script_path/bin/termdub.py -t dbms \
+  &
+do_sleep
+
+DUBS_TERMNAME="" \
+  DUBS_STARTIN="/ccp/etc/cp_confs" \
+  DUBS_STARTUP="" \
+  $script_path/bin/termdub.py -t dbms \
+  &
+do_sleep
+
+#
+DUBS_TERMNAME="" \
+  DUBS_STARTIN="" \
+  DUBS_STARTUP="sss runic" \
+  $script_path/bin/termdub.py -t dbms \
+  &
+do_sleep
+
+DUBS_TERMNAME="" \
+  DUBS_STARTIN="" \
+  DUBS_STARTUP="" \
+  $script_path/bin/termdub.py -t dbms \
+  &
+do_sleep
+
+#
+DUBS_TERMNAME="" \
+  DUBS_STARTIN="/ccp/dev/cp/pyserver" \
+  DUBS_STARTUP="sss runic" \
+  $script_path/bin/termdub.py -t dbms \
+  &
+do_sleep
+
+DUBS_TERMNAME="py" \
+  DUBS_STARTIN="/ccp/dev/cp/pyserver" \
+  DUBS_STARTUP="python" \
+  $script_path/bin/termdub.py -t dbms \
+  &
+do_sleep
+
+#
+# Skipping: $script_path/bin/termdub.py -t mini
+
---
+++ .fries/bin/termdub.py
@@ -0,0 +1,351 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# File: termdub.py
+# Author: Landon Bouma (dubsacks &#x40; retrosoft &#x2E; com)
+# Last Modified: 2015.01.23
+# Project Page: https://github.com/landonb/home_fries
+# License: GPLv3
+
+"""
+Monitor-size-aware Dimensionally-adaptive Gnome-terminal Wrapper
+================================================================
+
+Overview
+~~~~~~~~
+
+This script wraps gnome-terminal so you can open specifically-sized
+terminal windows that are fit for your monitor's screen size.
+
+Setup
+~~~~~
+
+Update Termdub.geoms with your screen resolution,
+if it's not already registered.
+
+Options
+~~~~~~~
+
+------------------------   --------------------------------------
+Terminal Window            Command
+Location
+or Dimensions
+========================   ======================================
+Left Half of Screen        path/to/this/script/termdub.py -t lhs
+Right Half of Screen       path/to/this/script/termdub.py -t rhs
+Top Half of Screen         path/to/this/script/termdub.py -t logs
+Bottom Half of Screen      path/to/this/script/termdub.py -t logc
+Large and Square Window    path/to/this/script/termdub.py -t dbms
+Miniature Window           path/to/this/script/termdub.py -t mini 
+------------------------   --------------------------------------
+
+Example Usage: As Panel Launchers
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Make panel launchers for those sizes you find
+useful, so you can easily open new terminals::
+
+   Gnome [menubar] 
+     < Applications 
+     < System Tools 
+     < Terminal [Right-click and choose] Add this launcher to Panel 
+
+Add one new launcher for each window size you like,
+and then right-click each new launcher and edit the
+Properties, entering the appropriate Command from
+the table above.
+
+"""
+
+import optparse
+import os
+import re
+import subprocess
+import sys
+import time
+
+# env DUBS_TERMNAME="" \
+#  env DUBS_STARTIN="" \
+#  env DUBS_STARTUP="" \
+#  $HOME/.fries/bin/termdub.py -t lhs
+
+class Termdub_Parser(optparse.OptionParser):
+
+   def __init__(self):
+      optparse.OptionParser.__init__(self)
+      self.cli_opts = None
+      self.cli_args = None
+
+   def get_opts(self):
+      self.prepare();
+      self.parse();
+      assert(self.cli_opts is not None)
+      return self.cli_opts
+
+   def prepare(self):
+      self.add_option('-t', '--target', dest='target',
+         action='store', default='lhs', type='choice',
+         choices=['lhs','rhs','logs','logc','dbms','mini',],
+         help='target: lhs, rhs, logs, logc, dbms, or mini')
+
+   # FIXME: Add
+   # DUBS_TERMNAME="" DUBS_STARTIN="" DUBS_STARTUP="" 
+   # to options, since the gnome shortcut keeps running 
+   # DUBS_STARTUP for the first window (winpdb)...
+   # and I can't pass an env var to termdub from the
+   # gnome applet, for whatever reason.
+
+   def parse(self):
+      '''Parse the command line arguments.'''
+      (opts, args) = self.parse_args()
+      # parse_args halts execution if user specifies:
+      #  (a) '-h', (b) '--help', or (c) unknown option.
+      self.cli_opts = opts
+      self.cli_args = args
+
+class Termdub(object):
+
+   XR_RE = re.compile(
+      r", current (?P<res_w>[0-9]+) x (?P<res_h>[0-9]+), maximum ")
+
+   # CAVEAT: This script fails if the monitor size is not programmed herein.
+   geoms = {
+      "lhs": {                   # These resolutions are for [lb's] monitors...
+         1280: {
+            800: "77x38+0+100",  # lenovo x201
+            1024: "77x46+0+100", # hp L1925
+            },
+         1600: {
+            1200: "97x56+0+100", # Dell ...
+            },
+         1680: {
+            1050: "97x52+30+20", # SyncMaster 226BW
+            },
+         },
+      "rhs": {
+         1280: {
+            800: "77x38+1000+100",
+            1024: "77x46+1000+100",
+            },
+         1600: {
+            1200: "97x56+1000+100",
+            },
+         1680: {
+            1050: "97x52+850+20",
+            },
+         },
+      "logs": {
+         1280: {
+            800: "1000x17+0+20",
+            1024: "1000x27+0+20",
+            },
+         1600: {
+            1200: "1000x32+0+20",
+            },
+         1680: {
+            1050: "1000x27+0+20",
+            },
+         },
+      "logc": {
+         1280: {
+            800: "1000x18+0+1000",
+            1024: "1000x21+0+1000",
+            },
+         1600: {
+            1200: "1000x26+0+1000",
+            },
+         1680: {
+            1050: "1000x22+0+535",
+            },
+         },
+      "dbms": {
+         1280: {
+            800: "100x38+150+100",
+            1024: "110x43+315+115",
+            },
+         1600: {
+            1200: "110x43+525+250",
+            },
+         1680: {
+            1050: "110x43+515+115",
+            },
+         },
+      "mini": {
+         1280: {
+            800: "77x30+250+100",
+            1024: "77x30+250+100",
+            },
+         1600: {
+            1200: "77x30+725+425",
+            },
+         1680: {
+            1050: "77x30+575+200",
+            },
+         },
+      }
+
+   def __init__(self):
+      pass
+
+   #
+   def go(self, target):
+      (width, height) = self.get_monitor_resolution()
+      # If the width and height don't match the known monitor sizes,
+      # use the next lowest size.
+      (width, height) = self.normalize_resolution(width, height, target)
+      self.open_terminal_window(width, height, target)
+
+   #
+   def get_monitor_resolution(self):
+      #width = 1280
+      #height = 1024
+      width = 64
+      height = 64
+      re.compile(r"^WARNING:"),
+      the_cmd = "xrandr"
+      # Python3.3 adds universal_newlines, which defaults to False and causes
+      # raw binary stream to be returned. But we want to read strings.
+      try:
+         p = subprocess.Popen([the_cmd], 
+                              shell=True, 
+                              # bufsize=bufsize,
+                              stdin=subprocess.PIPE, 
+                              stdout=subprocess.PIPE, 
+                              stderr=subprocess.STDOUT, 
+                              close_fds=True,
+                              universal_newlines=True)
+
+# FIXME: py 3 v 2 problem ,.... except TypeError as e:
+      except TypeError, e:
+         p = subprocess.Popen([the_cmd], 
+                              shell=True, 
+                              # bufsize=bufsize,
+                              stdin=subprocess.PIPE, 
+                              stdout=subprocess.PIPE, 
+                              stderr=subprocess.STDOUT, 
+                              close_fds=True)
+      (sin, sout_err) = (p.stdin, p.stdout)
+      while True:
+         line = sout_err.readline()
+         matched = self.XR_RE.search(line)
+         if not line:
+            print("WARNING: resolution not found!")
+            break
+         if matched is not None:
+            width = int(matched.group("res_w"))
+            height = int(matched.group("res_h"))
+            break
+      sin.close()
+      sout_err.close()
+      p.wait()
+      return (width, height)
+
+   #
+   def normalize_resolution(self, width, height, target):
+
+      # Check the width first.
+      widths = list(Termdub.geoms[target].keys())
+      widths.sort(reverse=True)
+      correct_width = 0
+      for known in widths:
+         if width == known:
+            correct_width = width
+            break
+         elif width > known:
+            correct_width = known
+            break
+      if not correct_width:
+         # Use the smallest width defined above.
+         correct_width = widths[-1]
+
+      # Now check the height.
+      heights = list(Termdub.geoms[target][correct_width].keys())
+      heights.sort(reverse=True)
+      correct_height = 0
+      for known in heights:
+         if height == known:
+            correct_height = height
+            break
+         elif width > known:
+            correct_height = known
+            break
+      if not correct_height:
+         # Use the smallest height defined for this width.
+         correct_height = heights[-1]
+
+      return (correct_width, correct_height)
+
+   #
+   def open_terminal_window(self, width, height, target):
+
+      # NOTE: We gotta be in the user's home directory for gnome-terminal to
+      # source our bash startup scripts.
+      os.chdir(os.getenv('HOME'))
+
+      # The MATE window manager renames its GNOMEy derivatives.
+      try:
+         wind_mgr = subprocess.check_output(
+                     ['/usr/bin/wmctrl', '-m',],
+                     stderr=subprocess.STDOUT)
+      except TypeError, e:
+         wind_mgr = subprocess.check_output(
+                     ['/usr/bin/wmctrl', '-m',],
+                     stderr=subprocess.STDOUT,
+                     universal_newlines=True)
+      if re.match(r"^Name: Mutter (Muffin)\n", wind_mgr):
+         # Cinnamon
+         the_terminal = 'gnome-terminal'
+      #elif re.match(r"^Name: Xfwm4\n", wind_mgr):
+      #   # Xfce
+      #   [lb] is not sure this is right...
+      #   the_terminal = 'gnome-terminal'
+      elif (re.match(r"^Name: Marco\n", wind_mgr)
+            or re.match(r"^Name: Metacity \(Marco\)\n", wind_mgr)):
+         # MATE
+         the_terminal = 'mate-terminal'
+      else:
+         print('WARNING: Unknown OS: Is this GNOME or MATE?')
+         #sys.exit(1)
+         the_terminal = 'gnome-terminal'
+
+      # NOTE: The -e/--command runs a command inside the new terminal, but it's
+      #       before bashrc executes, and a Ctrl-C closes the window. So if you
+      #       want to run a command inside the terminal window and then let the
+      #       terminal window live independent of that process, don't use -e.
+      # So we don't use -e/--command, but we can use -t/--title, which names
+      # the window first before bashrc runs the command, since gnome-terminal
+      # doesn't update its titlebar until after bashrc completes.
+
+      termname = os.getenv('DUBS_TERMNAME')
+      if termname is not None:
+         termname = '--title "%s"' % (termname,)
+      else:
+         termname = ''
+
+      #
+      print('Opening %s: %s: %d x %d.'
+            % (the_terminal, target, width, height,))
+      #
+      the_cmd = ('%s %s --geometry %s' 
+                 % (the_terminal,
+                    termname,
+                    Termdub.geoms[target][width][height],))
+      #
+      try:
+         p = subprocess.Popen(the_cmd, shell=True, universal_newlines=True)
+      except TypeError, e:
+         p = subprocess.Popen(the_cmd, shell=True)
+      sts = os.waitpid(p.pid, 0)
+
+# ***
+
+if (__name__ == "__main__"):
+
+   parser = Termdub_Parser()
+   cli_opts = parser.get_opts()
+
+   tdub = Termdub()
+   tdub.go(cli_opts.target)
+
+# vim:tw=0:ts=3:sw=3:et:norl:
+
