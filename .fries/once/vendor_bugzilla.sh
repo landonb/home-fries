09848314ab7c3c4be8ca9feae53a10db6d8cc814
Landon Bouma <landonb@users.noreply.github.com>
2015-03-18 21:43:12 -0500

    Fix/revert mis-AutoAdapation mal-updated 'Last Modified'.

--- .fries/once/vendor_bugzilla.sh
+++ .fries/once/vendor_bugzilla.sh
@@ -1,6 +1,6 @@
 # File: vendor_bugzilla.sh
 # Author: Landon Bouma (landonb &#x40; retrosoft &#x2E; com)
-# Last Modified: 2015.03.01
+# Last Modified: 2015.02.02
 # Project Page: https://github.com/landonb/home_fries
 # Summary: Bugzilla setup script.
 # License: GPLv3
