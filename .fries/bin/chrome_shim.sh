f02ea9489b905a42bc74b781fa49b1ef004bb953
Landon Bouma <landon.bouma@gmail.com>
2016-05-02 22:27:18 -0500

    Enhance the new chrome-opener shim.
    - Need to move to correct monitor in case it went
      to hidden dead space created by two monitors
      arranged diagonally from one another.

--- .fries/bin/chrome_shim.sh
+++ .fries/bin/chrome_shim.sh
@@ -18,14 +18,33 @@
 #  https://productforums.google.com/forum/?hl=en#!topic/chrome/FA98j4se-Go;context-place=topicsearchin/chrome/new$20windows$20does$20not$20get$20focus
 
 # See: https://standards.freedesktop.org/desktop-entry-spec/latest/index.html
+#
 #  %U: A list of URLs. Each URL is passed as a separate argument to the executable program.
 #      Local files may either be passed as file: URLs or as file path.
 #      - I don't think we need to worry about %U, do we?
 #        - We're not sending any arguments to the launcher.
+#
 # Original launcher command:
+#
 #  /usr/bin/google-chrome-stable %U
+#
 /usr/bin/google-chrome-stable &
+
 # 2016-05-02: I tried 0.1/didn't work and 1.0/worked, and 0.5/worked/compromise.
 sleep 0.5
-wmctrl -a "New Tab - Google Chrome"
+
+# This maximizes the new tab window:
+#
+#   wmctrl -a "New Tab - Google Chrome"
+#
+# but it only works if the topmost existing Chrome window is in the
+# upper monitor, if the second monitor is bottom-left (because the
+# new windows gets the same left/x value as the topmost Chrome window
+# by a 0-value y. So, at home, if the topmost Chrome window is in the
+# bottom-left monitor, the new Chrome window goes to the upper-left
+# hidden area.e
+#
+# hp L1925 1280x1024 / ASUS VS228H-P 1920x1080
+wmctrl -a "New Tab - Google Chrome" -e 0,1280,0,1920,1080
+wmctrl -a "New Tab - Google Chrome" -b add,maximized_vert,maximized_horz
 
