e15361ca83825ef8a90603d4a246c1920d9452e1
Landon Bouma <landon.bouma@gmail.com>
2016-05-04 20:59:14 -0500

    chrome_shim betterment.
    - Use window IDs instead of search string, so
      we can iterate over all matches.
    - I think I fixed the race condition after
      starting Chrome with a good old fashioned
      sleep.

--- .fries/bin/chrome_shim.sh
+++ .fries/bin/chrome_shim.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Last Modified: 2016-05-03
+# Last Modified: 2016-05-04
 
 # For years I've dealt with a weird Chrome issue that I never figured out
 # how to solve on my own -- until now! -
@@ -30,24 +30,51 @@
 #
 /usr/bin/google-chrome-stable &
 
-# 2016-05-02: I tried 0.1/didn't work and 1.0/worked, and 0.5/sometimes worked.
-sleep .666
+# If we don't sleep, wmctrl -l won't see the new window.
+sleep .5
+# 2016.05.04: I thought I had timing issues between wmctrl
+# commands, too, but I think that was something else; so
+# sleep is disable for remaining command transitions.
+SLEEP_TIME="0"
 
-# FIXME: This should not be hardcoded in home-fries. Move to waffle.
-if [[ $(hostname) == 'larry' ]]; then
-  # If you have multiple monitors, a new Chrome window will be positioned
-  # with y=0 and x=same as the x of the topmost Chrome window. As such,
-  # if you have two monitors positioned diagonally, if the topmost chrome
-  # window is on the bottom monitor, new chrome windows will be placed in
-  # the dead (offscreen) space above it.
-  #
-  # To fix this problem, we have to know the dimensions of both monitors.
-  #
-  # hp L1925 1280x1024 / ASUS VS228H-P 1920x1080
-  wmctrl -a "New Tab - Google Chrome" -e 0,1280,0,1920,1080
-  wmctrl -a "New Tab - Google Chrome" -b add,maximized_vert,maximized_horz
-else
-  # Maximize the new tab window.
-  wmctrl -a "New Tab - Google Chrome"
-fi
+# Process ID? is shared:
+#  wmctrl -p -l | grep "New Tab - Google Chrome" | awk '{print $3}'
+# But whatever the first value is is unique:
+#  wmctrl -p -l | grep "New Tab - Google Chrome" | awk '{print $1}'
+
+WINDOW_IDS=$(wmctrl -p -l | grep "New Tab - Google Chrome" | awk '{print $1}')
+
+for window_id in $WINDOW_IDS; do
+
+  # FIXME: This should not be hardcoded in home-fries. Move to waffle.
+  if [[ $(hostname) == 'larry' ]]; then
+    # If you have multiple monitors, a new Chrome window will be positioned
+    # with y=0 and x=same as the x of the topmost Chrome window. As such,
+    # if you have two monitors positioned diagonally, if the topmost chrome
+    # window is on the bottom monitor, new chrome windows will be placed in
+    # the dead (offscreen) space above it.
+    #
+    # To fix this problem, we have to know the dimensions of both monitors.
+    #
+    # hp L1925 1280x1024 / ASUS VS228H-P 1920x1080
+    sleep $SLEEP_TIME
+    wmctrl -i -a $window_id -b remove,maximized_vert,maximized_horz
+    sleep $SLEEP_TIME
+    wmctrl -i -a $window_id -e 0,1280,0,1920,1080
+    # It doesn't always catch the first time.
+    sleep $SLEEP_TIME
+    wmctrl -i -a $window_id -b add,maximized_vert,maximized_horz
+    sleep $SLEEP_TIME
+    wmctrl -i -a $window_id
+  else
+    # Maximize the new tab window.
+    #wmctrl -a "New Tab - Google Chrome"
+    sleep $SLEEP_TIME
+    wmctrl -i -a $window_id
+    sleep $SLEEP_TIME
+    wmctrl -i -a $window_id -b add,maximized_vert,maximized_horz
+    sleep $SLEEP_TIME
+    wmctrl -i -a $window_id
+  fi
+done
 
