d5f3f9adf2567eb7e029e24689f083285c936a30
Landon Bouma <landon.bouma@gmail.com>
2016-08-29 13:13:12 -0500

    Colorful `man` pages.

--- .fries/.bashrc/bashrc.core.sh
+++ .fries/.bashrc/bashrc.core.sh
@@ -1,6 +1,6 @@
 # File: bashrc.core.sh
 # Author: Landon Bouma (landonb &#x40; retrosoft &#x2E; com)
-# Last Modified: 2016.08.16
+# Last Modified: 2016.08.17
 # Project Page: https://github.com/landonb/home_fries
 # Summary: One Developer's Bash Profile
 # License: GPLv3
@@ -1843,6 +1843,22 @@ export font_underline_bash="\033[4m"
 export HISTTIMEFORMAT="%d/%m/%y %T "
 
 #########################
+
+# "Want colored man pages?"
+# http://boredzo.org/blog/archives/2016-08-15/colorized-man-pages-understood-and-customized
+# https://superuser.com/questions/452034/bash-colorized-man-page
+
+man() {
+  env \
+    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+    LESS_TERMCAP_md=$(printf "\e[1;31m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;32m") \
+    man "$@"
+}
 
 ############################################################################
 # DONE                              DONE                              DONE #
