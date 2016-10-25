64804f97ed6c1dfd67149969be38a0a5b7663637
Landon Bouma <landon.bouma@gmail.com>
2016-10-24 19:58:16 -0500

    Add docker_ps command to docker_util.sh.

--- .fries/lib/docker_util.sh
+++ .fries/lib/docker_util.sh
@@ -1,6 +1,6 @@
 # File: .fries/lib/docker_util.sh
 # Author: Landon Bouma (landonb &#x40; retrosoft &#x2E; com)
-# Last Modified: 2016.10.20
+# Last Modified: 2016.10.24
 # Project Page: https://github.com/landonb/home-fries
 # Summary: Docker Helpers: I'm new to Docker, Fall, 2016. These are my bash.
 # License: GPLv3
@@ -11,6 +11,9 @@ docker_remove_exited () {
   docker rm -v $(docker ps -a -q -f status=exited)
 # 2016-10-20: The remove-exited containers did not remove any project containers.
 #             I expected otherwise...
+  # Stop and remove all docker containers
+  # docker stop $(docker ps -a -q)
+  # docker rm $(docker ps -a -q)
 }
 
 docker_remove_dangling () {
@@ -32,5 +35,30 @@ docker_list_ips () {
       echo -n -e "$cid\t"
       docker inspect --format "{{ .Name }} @ {{ .NetworkSettings.IPAddress }}" $cid
     done
+}
+
+docker_ps () {
+
+  # "Valid placeholders for the Go template are listed below:"
+  #   .ID	Container ID
+  #   .Image	Image ID
+  #   .Command	Quoted command
+  #   .CreatedAt	Time when the container was created.
+  #   .RunningFor	Elapsed time since the container was started.
+  #   .Ports	Exposed ports.
+  #   .Status	Container status.
+  #   .Size	Container disk size.
+  #   .Names	Container names.
+  #   .Labels	All labels assigned to the container.
+  #   .Label	Value of a specific label for this container. For example '{{.Label "com.docker.swarm.cpu"}}'
+  #   .Mounts
+  # As of 2016-10-24.
+  # https://docs.docker.com/engine/reference/commandline/ps/#formatting
+
+  #  docker ps --format "table {{.ID}}\t{{.Labels}}"
+  #  docker ps --format "table {{.ID}}\t{{.Image}}\t{{.Command}}\t{{.CreatedAt}}\t{{.RunningFor}}\t{{.Ports}}\t{{.Status}}\t{{.Size}}\t{{.Names}}\t{{.Labels}}\t{{.Mounts}}"
+  #docker ps --format "table {{.Image}}\t{{.Command}}\t{{.RunningFor}}\t{{.Ports}}\t{{.Status}}\t{{.Names}}"
+  docker ps --format "table {{.Image}}\t{{.Command}}\t{{.Ports}}\t{{.Status}}\t{{.Names}}"
+
 }
 
