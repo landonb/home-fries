93744dff3acab439640a5ad0b8f99ebe0051e5d3
Landon Bouma <landon.bouma@gmail.com>
2016-11-02 18:36:22 -0500

    packme.sh now call travel with -WW wait, wait check in my files please flag

--- .fries/recipe/bin/packme.sh
+++ .fries/recipe/bin/packme.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #SCRIPT_DIR=$(pwd -P)
 SCRIPT_DIR="$(dirname ${BASH_SOURCE[0]})"
-${SCRIPT_DIR}/travel packme $*
+#${SCRIPT_DIR}/travel packme $*
+# 2016-11-02: That's the ticket! Auto checkin known dirty git.
+${SCRIPT_DIR}/travel packme -WW $*
 
