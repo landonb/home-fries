52037b96369c639781e903c33b604cce27525fc2
Landon Bouma <landon.bouma@gmail.com>
2016-11-11 17:23:38 -0600

    Add bundle_ setup script (start of one, at least).

---
+++ .fries/once/custom_setup.vim.bundle_.sh
@@ -0,0 +1,26 @@
+
+# FIXME: This should be in recipe/, I wonder...
+
+clone_repos() {
+
+	cd ${HOME}/.vim/bundle_
+
+	git clone ssh://git@github.com/landonb/dubs_appearance.git
+	git clone ssh://git@github.com/landonb/dubs_buffer_fun.git
+	git clone ssh://git@github.com/landonb/dubsacks_vim.git
+	git clone ssh://git@github.com/landonb/dubs_edit_juice.git
+	git clone ssh://git@github.com/landonb/dubsacks_vim.git
+	git clone ssh://git@github.com/landonb/dubs_file_finder.git
+	git clone ssh://git@github.com/landonb/dubs_ftype_mess.git
+	git clone ssh://git@github.com/landonb/dubs_grep_steady.git
+	git clone ssh://git@github.com/landonb/dubs_html_entities.git
+	git clone ssh://git@github.com/landonb/dubs_project_tray.git
+	git clone ssh://git@github.com/landonb/dubs_quickfix_wrap.git
+	git clone ssh://git@github.com/landonb/dubs_style_guard.git
+	git clone ssh://git@github.com/landonb/dubs_syntastic_wrap.git
+	git clone ssh://git@github.com/landonb/dubs_toggle_textwrap.git
+
+}
+
+clone_repos
+
