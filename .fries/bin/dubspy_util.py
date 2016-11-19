734a921c80dc3d2902bc46db5523f3212aeb5630
Landon Bouma <landon.bouma@gmail.com>
2016-11-19 15:43:11 -0600

    Comments and whitespace.

--- .fries/bin/dubspy_util.py
+++ .fries/bin/dubspy_util.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # vim: ts=4:sw=4:et
 
 # File: termdub.py
 # Author: Landon Bouma (landonb &#x40; retrosoft &#x2E; com)
-# Last Modified: 2016.05.04
+# Last Modified: 2016.11.19
 # Project Page: https://github.com/landonb/home_fries
 # License: GPLv3
 
@@ -22,14 +22,15 @@ class Term_Util(object):
     @staticmethod
     def get_emulator_app_name():
         # The MATE window manager renames its GNOMEy derivatives.
-        # 2015.08.07: There used to be a try-except where we'd try without
-        # univeral_newlines and catch TypeError if it failed, and then we'd
-        # try univeral_newlines=True. But it should always be True because
-        # we want a string, not bytes.
+        # 2015.08.07: There used to be a try-except where we'd try
+        #  without universal_newlines and catch TypeError if it
+        #  failed, and then we'd try universal_newlines=True. But it
+        #  should always be True because we want a string, not bytes.
         wind_mgr = subprocess.check_output(
-        ['/usr/bin/wmctrl', '-m',],
-        stderr=subprocess.STDOUT,
-        universal_newlines=True)
+            ['/usr/bin/wmctrl', '-m',],
+            stderr=subprocess.STDOUT,
+            universal_newlines=True
+        )
         if re.match(r"^Name: Mutter (Muffin)\n", wind_mgr):
             # Cinnamon
             the_terminal = 'gnome-terminal'
