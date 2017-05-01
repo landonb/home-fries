559ed286a27012b7d76d8b1005722db82143b43e
Landon Bouma <landon.bouma@gmail.com>
2017-05-01 18:31:39 -0500

    Bump go vers, from 1.8rc2 to 1.8.1.

--- .fries/once/custom_setup.extras.sh
+++ .fries/once/custom_setup.extras.sh
@@ -3,7 +3,7 @@
 
 # File: custom_setup.extras.sh
 # Author: Landon Bouma (landonb &#x40; retrosoft &#x2E; com)
-# Last Modified: 2017.04.25
+# Last Modified: 2017.05.01
 # Project Page: https://github.com/landonb/home_fries
 # Summary: Third-party tools downloads compiles installs.
 # License: GPLv3
@@ -4842,7 +4842,8 @@ stage_4_go_get_go () {
   #sudo apt-get upgrade
   #sudo apt-get dist-upgrade
 
-  GO_VERS="go1.8rc2"
+  #GO_VERS="go1.8rc2"
+  GO_VERS="go1.8.1"
   GO_TBAL="${GO_VERS}.linux-amd64.tar.gz"
   wget -N https://storage.googleapis.com/golang/${GO_TBAL}
   wget -N https://storage.googleapis.com/golang/${GO_TBAL}.sha256
