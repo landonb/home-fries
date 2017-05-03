203133964855991d40d38b72ff5b1487fefae403
Landon Bouma <landon.bouma@gmail.com>
2017-05-03 11:44:47 -0500

    Modernize making of GEM_PATH.

--- .fries/lib/ruby_util.sh
+++ .fries/lib/ruby_util.sh
@@ -1,6 +1,6 @@
 # File: ruby_util.sh
 # Author: Landon Bouma (landonb &#x40; retrosoft &#x2E; com)
-# Last Modified: 2017.02.25
+# Last Modified: 2017.05.03
 # Project Page: https://github.com/landonb/home_fries
 # Summary: Ruby Helpers.
 # License: GPLv3
@@ -83,15 +83,48 @@ chruby_use () {
 }
 
 ruby_set_gem_path () {
-  RUBY_MINOR_ZERO=$(ruby -e "puts RUBY_VERSION.split('.')[0..1].join('.') + '.0'")
-  GEM_PATH="${GEM_PATH}:${HOME}/.gem/ruby/${RUBY_MINOR_ZERO}"
-  GEM_PATH="${GEM_PATH}:${HOME}/.rubies/ruby-${RUBY_MINOR_ZERO}/lib/ruby/gems/${RUBY_MINOR_ZERO}"
+  local GEM_PATHS=()
+
+  if ``command -v ruby >/dev/null 2>&1``; then
+    RUBY_MINOR_ZERO=$(ruby -e "puts RUBY_VERSION.split('.')[0..1].join('.') + '.0'")
+    GEM_PATHS+=(${HOME}/.gem/ruby/${RUBY_MINOR_ZERO})
+    # 2017-05-03: MAYBE: Do we need this one, too? If anything, it's same as previous.
+    GEM_PATHS+=($(ruby -rubygems -e 'puts Gem.user_dir'))
+  fi
+
+  GEM_PATHS+=(${HOME}/.rubies/ruby-${RUBY_MINOR_ZERO}/lib/ruby/gems/${RUBY_MINOR_ZERO})
+
   # 2017-01-25: Haven't touched a project in one month, and now it's not working?
   #   Am I on a different machine, or what? Anyway, missing /var/lib/gems, I guess!
-  if [[ -d /var/lib/gems/${RUBY_MINOR_ZERO} ]]; then
-    GEM_PATH="${GEM_PATH}:/var/lib/gems/${RUBY_MINOR_ZERO}"
-  fi
+  GEM_PATHS+=(/var/lib/gems/${RUBY_MINOR_ZERO})
+
+  for ((i = 0; i < ${#GEM_PATHS[@]}; i++)); do
+    PATH_ELEM="${GEM_PATHS[$i]}"
+    if [[ -d "${PATH_ELEM}" ]]; then
+      if [[ ":${GEM_PATH}:" != *":${PATH_ELEM}:"* ]]; then
+        if [[ -n ${GEM_PATH} ]]; then
+          GEM_PATH="${GEM_PATH}:"
+        fi
+        GEM_PATH="${GEM_PATH}${PATH_ELEM}"
+      fi
+    fi
+  done
+
   export GEM_PATH
+
+  # $ echo $GEM_PATH
+  # ${HOME}/.gem/ruby/2.3.0:/var/lib/gems/2.3.0
+  # $ gogo project
+  # Skipping ~/.exoline symlink: no replacement found.
+  # Entered /work/clients/project
+  # $ ll
+  # Ignoring byebug-9.0.6 because its extensions are not built.  Try: gem pristine byebug --version 9.0.6
+  # Monkey patching!
+  # total 188K
+  # drwxrwxr-x 11 landonb landonb 4.0K May  1 18:41 ./
+  # drwxrwxr-x  7 landonb landonb 4.0K May  1 14:08 ../
+  # ...
+
 }
 ruby_set_gem_path
 
