469d6609c649c2d956df19ec1ca782fb7a269458
landonb <landonb@users.noreply.github.com>
2017-08-25 13:15:57 -0500

    Comment.

--- .fries/.bashrc/bashrc.core.sh
+++ .fries/.bashrc/bashrc.core.sh
@@ -1,6 +1,6 @@
 # File: bashrc.core.sh
 # Author: Landon Bouma (landonb &#x40; retrosoft &#x2E; com)
-# Last Modified: 2017.08.14
+# Last Modified: 2017.08.25
 # Project Page: https://github.com/landonb/home_fries
 # Summary: One Developer's Bash Profile [Home-🍟]
 # License: GPLv3
@@ -1786,6 +1786,8 @@ touchpad_enable () {
 
 #########################
 
+# Helpful Bash Tab Completion aliases.
+
 # For those (silly) projects that use tabs (I know!) in Bash scripts,
 # you'll want to disable tab autocompletion if you want to copy-and-paste
 # from the script to the terminal, otherwise the autocomplete responses
@@ -1796,6 +1798,18 @@ touchpad_enable () {
 
 alias toff="bind 'set disable-completion on'"
 alias ton="bind 'set disable-completion off'"
+
+# 2017-08-25: You need to disable tab completion to test copy-paste
+#             ``<<-`` here-documents.
+#
+#             E.g.,
+#
+#                 bind 'set disable-completion on'
+#
+#                 cat <<-EOF
+#                 	<Tabdented>
+#                 EOF
+#
 
 #########################
 
