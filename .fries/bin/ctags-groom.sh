af0b60bdb0a90a216d0563a434310514eff4e4df
landonb <landonb@users.noreply.github.com>
2017-10-17 13:14:36 -0500

    Add Style (indenting) to (file previously copied from someone else,) ctags-groom.

--- .fries/bin/ctags-groom.sh
+++ .fries/bin/ctags-groom.sh
@@ -9,22 +9,28 @@ ctags "$@"
 
 FILE="tags"
 
-while [[ $# > 1 ]]
-do
-key="$1"
-
-case $key in
+while [[ $# > 1 ]]; do
+  key="$1"
+  case $key in
     -f)
-    FILE="$2"
-    shift
-    ;;
-esac
-shift
+      FILE="$2"
+      shift
+      ;;
+  esac
+  shift
 done
 
+echo "FILE: ${FILE}"
+
 # Filter out false matches from class method regex
-sed -i '' -E '/^(if|switch|function|module\.exports|it|describe)	.+language:js$/d' $FILE
+echo /bin/sed \
+  -i '' \
+  -E '/^(if|switch|function|module\.exports|it|describe)	.+language:js$/d' \
+  ${FILE}
 
 # Filter out false matches from object definition regex
-sed -i '' -E '/var[ 	]+[a-zA-Z0-9_$]+[ 	]+=[ 	]+require\(.+language:js$/d' $FILE
+echo /bin/sed \
+  -i '' \
+  -E '/var[ 	]+[a-zA-Z0-9_$]+[ 	]+=[ 	]+require\(.+language:js$/d' \
+  ${FILE}
 
