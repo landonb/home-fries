ea1f745a5335b012b2ae55b7c03872a85ce040fa
landonb <landonb@users.noreply.github.com>
2018-06-04 14:49:00 -0500

    Enhancement: Add startup function to run commands in new Terminator window panes, and to keep sheel around on Ctrl-C.
    - Generally, Terminator custom commands run until Ctrl-C,
      and then the pane closes. This option -- using an INIT_CMD
      environ -- dumps the user to a shell prompt on Ctrl-C,
      rather than closing pane.

--- .fries/.bashrc/bashrc.base.sh
+++ .fries/.bashrc/bashrc.base.sh
@@ -239,6 +239,40 @@ home_fries_bashrc_cleanup() {
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
+# Thanks, Amir Rachum, for the magic!
+#   https://amir.rachum.com/blog/2015/11/28/terminator-multiple-custom-commands/
+# This gets past a Terminator behavior where it's hard to run a custom
+# command that sets up an interactive shell. So you can, e.g., use this
+# as your Terminator custom command:
+#   env INIT_CMD="cd bla; export PYTHONPATH=/tmp; workon project" zsh
+#
+# NOTE: (lb): I copied a zsh function and updated it.
+home_fries_run_terminator_init_cmd() {
+  if [[ -n "${INIT_CMD}" ]]; then
+    echo ${INIT_CMD}
+    OLD_IFS=$IFS
+    # (lb): setopt, and shwordsplit, and Z Shell-specific, to emulate Bash behavior.
+    #setopt shwordsplit
+    IFS=';'
+    local cmd
+    for cmd in ${INIT_CMD}; do
+      # (lb): Is `print` how you add to history in Z Shell? I get an error.
+      #
+      #   $ /usr/bin/print
+      #   Unescaped left brace in regex is deprecated, passed through in regex;
+      #   marked by <-- HERE in m/%{ <-- HERE (.*?)}/ at /usr/bin/print line 528.
+      #
+      #print -s "${cmd}"  # add to history
+      history -s "${cmd}"  # add to history
+      eval "${cmd}"
+    done
+    unset INIT_CMD
+    IFS=${OLD_IFS}
+  fi
+}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
 main() {
   source_deps
   unset source_deps
@@ -265,6 +299,9 @@ main() {
 
   home_fries_bashrc_cleanup
   unset home_fries_bashrc_cleanup
+
+  home_fries_run_terminator_init_cmd
+  unset home_fries_run_terminator_init_cmd
 }
 
 main "$@"
