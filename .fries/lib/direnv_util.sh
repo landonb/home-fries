b1602420b0b8d51aac349fde8b98d01d69e14706
landonb <landonb@users.noreply.github.com>
2018-06-26 15:37:01 -0500

    Feature: Direnv export-function function.

---
+++ .fries/lib/direnv_util.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# vim:tw=0:ts=2:sw=2:et:norl:
+
+# File: session_util.sh
+# Author: Landon Bouma (landonb &#x40; retrosoft &#x2E; com)
+# Project Page: https://github.com/landonb/home-fries
+# Summary: Generic Bash function library.
+# License: GPLv3
+
+# Usage: Source this script. Call its functions. Use its exports.
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# "Picking up aliases and functions". Via:
+#  https://github.com/direnv/direnv/issues/73
+direnv_export() {
+  local name=$1
+  local alias_dir=$PWD/.direnv/aliases
+  mkdir -p "$alias_dir"
+  PATH_add "$alias_dir"
+  local target="$alias_dir/$name"
+  if declare -f "$name" >/dev/null; then
+    echo "#!/usr/bin/env bash" > "$target"
+    declare -f "$name" >> "$target" 2>/dev/null
+    # Notice that we add shell variables to the function trigger.
+    echo "$name \$*" >> "$target"
+    chmod +x "$target"
+  fi
+}
+
