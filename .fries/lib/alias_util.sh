1896aca7ea78c4b9d9229ba7840090e763e543d6
landonb <landonb@users.noreply.github.com>
2018-12-26 13:45:41 -0600

    Improve: De-distro'ize py3 alias; and use `command -v` to check for python3, not hardcoded path.

--- .fries/lib/alias_util.sh
+++ .fries/lib/alias_util.sh
@@ -138,18 +138,17 @@ home_fries_create_aliases_general () {
 
   # *** Python
 
-  # Alias python from py, but only if there's not already symlink.
-  if [[ ! -e /usr/bin/py ]]; then
-    alias py='/usr/bin/env python'
+  # [lb]: Python aliases for my lazy fingers.
+  if ! command -v py &> /dev/null; then
+    # [lb]: 2018-12-26: By convention, py should probably
+    # run python2, but lately I've been living dangerously.
+    alias py='/usr/bin/env python3'
   fi
-  if [[ ! -e /usr/bin/py2 ]]; then
+  if ! command -v py2 &> /dev/null; then
     alias py2='/usr/bin/env python2'
   fi
-  if [[ ! -e /usr/bin/py3 ]]; then
-    # 2016-11-28: hamster-briefs uses py3.5's subprocess.run.
-    #alias py3='/usr/bin/env python3'
-    # Argh...
-    alias py3='/usr/bin/env python3.5'
+  if ! command -v py3 &> /dev/null; then
+    alias py3='/usr/bin/env python3'
   fi
 
   # *** Ruby
