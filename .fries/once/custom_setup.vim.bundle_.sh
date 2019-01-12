02b56e9116d9613845520ebf9b16f1a581725c9d
landonb <landonb@users.noreply.github.com>
2019-01-12 15:04:26 -0600

    Update: Add new dubs_web_hatch to Dubs Vim submodule setup script; and add other missing projects.

--- .fries/once/custom_setup.vim.bundle_.sh
+++ .fries/once/custom_setup.vim.bundle_.sh
@@ -2,9 +2,9 @@
 # FIXME: This should be in recipe/, I wonder...
 
 clone_repos () {
-
 	cd ${HOME}/.vim/bundle_
 
+	git clone ssh://git@github.com/landonb/dubs_after_dark.git
 	git clone ssh://git@github.com/landonb/dubs_appearance.git
 	git clone ssh://git@github.com/landonb/dubs_buffer_fun.git
 	git clone ssh://git@github.com/landonb/dubs_edit_juice.git
@@ -12,11 +12,14 @@ clone_repos () {
 	git clone ssh://git@github.com/landonb/dubs_ftype_mess.git
 	git clone ssh://git@github.com/landonb/dubs_grep_steady.git
 	git clone ssh://git@github.com/landonb/dubs_html_entities.git
+	git clone ssh://git@github.com/landonb/dubs_mescaline.git
 	git clone ssh://git@github.com/landonb/dubs_project_tray.git
 	git clone ssh://git@github.com/landonb/dubs_quickfix_wrap.git
+	git clone ssh://git@github.com/landonb/dubs_rest_fold.git
 	git clone ssh://git@github.com/landonb/dubs_style_guard.git
 	git clone ssh://git@github.com/landonb/dubs_syntastic_wrap.git
 	git clone ssh://git@github.com/landonb/dubs_toggle_textwrap.git
+	git clone ssh://git@github.com/landonb/dubs_web_hatch.git
 	git clone ssh://git@github.com/landonb/vim-markdown.git
 
 }
