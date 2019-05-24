1370aa289c87c723f10af02f6ed3142b60027797
landonb <landonb@users.noreply.github.com>
2019-05-24 17:48:25 -0500

    Bugfix: Move chruby monkey patch to function, to ensure chruby sourced first.

--- .fries/lib/ruby_util.sh
+++ .fries/lib/ruby_util.sh
@@ -172,64 +172,68 @@ ruby_set_gem_path () {
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
-# Here we monkey patch the chruby function -- we replace
-# the chruby fcn. with our own wrapper function.
-# MAYBE: I should probably just submit a pull request.
-#
-# PROBLEM: If the ruby version is not the 0 patch level (e.g., 2.3.0),
-#   chruby doesn't include all the gem paths. For example, should you
-#   `chruby 2.3.3`, GEM_PATH includes the 2.3.3/ directory, but gem install
-#   stashed everything under the 2.3.0/ directory, so we have to add that
-#   back in.
+patch_export_chruby_use () {
+  # Here we monkey patch the chruby function -- we replace
+  # the chruby fcn. with our own wrapper function.
+  # MAYBE: I should probably just submit a pull request.
+  #
+  # PROBLEM: If the ruby version is not the 0 patch level (e.g., 2.3.0),
+  #   chruby doesn't include all the gem paths. For example, should you
+  #   `chruby 2.3.3`, GEM_PATH includes the 2.3.3/ directory, but gem install
+  #   stashed everything under the 2.3.0/ directory, so we have to add that
+  #   back in.
 
-# Do a little Bash trickery: Spit out the original function and set it up
-#   under a new name. We use `tail -n +2` to remove the original function
-#   name but leave the function body, e.g., leave out ``chruby_use ()\n``.
+  # Do a little Bash trickery: Spit out the original function and set it up
+  #   under a new name. We use `tail -n +2` to remove the original function
+  #   name but leave the function body, e.g., leave out ``chruby_use ()\n``.
 
-orig_chruby_use () {
-  :
-}
-if declare -f chruby_use &> /dev/null; then
-  eval "$(echo "orig_chruby_use()"; declare -f chruby_use | tail -n +2)"
-else
-  $HOMEFRIES_WARNINGS && echo "WARNING: chruby_use() not found"
-fi
-
-# And here's our
-#   MONKEY PATCH!
-chruby_use () {
-  orig_chruby_use $*
-  # See chruby_use in
-  #   /usr/local/share/chruby/chruby.sh
-  #   ${HOME}/.local/share/chruby/chruby.sh
-  # MAYBE: If you need to cleanup old paths, something like this:
-  if false; then
-    export GEM_PATH="$(\
-      echo ${GEM_PATH} \
-        | /bin/sed -r "s@:?${GEM_HOME}[^:]*:@:@g" \
-        | /bin/sed -r s/^:// \
-        | /bin/sed -r s/:$//
-    )"
+  orig_chruby_use () {
+    :
+  }
+  if declare -f chruby_use &> /dev/null; then
+    eval "$(echo "orig_chruby_use()"; declare -f chruby_use | tail -n +2)"
+  else
+    $HOMEFRIES_WARNINGS && echo "WARNING: chruby_use() not found"
   fi
-  # Check if patch version.
-  PATCH_NUM=$(ruby -e "puts RUBY_VERSION.split('.')[2]")
-  if [[ ${PATCH_NUM} -gt 0 ]]; then
-    # NOTE/2016-12-11: If there's a .ruby-version file in the current
-    #   directory, running *any* command might invoke us, since auto.sh
-    #   sets a trap on DEBUG which runs before every command. Just FYI.
-    # MAYBE: Silence this echo. For now, curious when this fcn. is triggered.
-    echo "Monkey patching!"
-    ruby_set_gem_path
-    # WRONG:
-    #RUBY_ROOT_ZERO=$(echo ${RUBY_ROOT} | /bin/sed -r s/-${RUBY_VERSION}$/-${RUBY_MINOR_ZERO}/)
-    #export PATH="${PATH}:${RUBY_ROOT_ZERO}/bin"
-    #export PATH="${PATH}:${GEM_HOME/${RUBY_VERSION}/${RUBY_MINOR_ZERO}}/bin"
-    local gem_ruby_bin="${GEM_HOME/${RUBY_VERSION}/${RUBY_MINOR_ZERO}}/bin"
-    #if [[ ":${PATH}:" != *":${gem_ruby_bin}:"* ]]; then
-    #  export PATH="${PATH}:${gem_ruby_bin}"
-    #fi
-    path_add_part ${gem_ruby_bin}
-  fi
+
+  # And here's our
+  #   MONKEY PATCH!
+  chruby_use () {
+    orig_chruby_use $*
+    # See chruby_use in
+    #   /usr/local/share/chruby/chruby.sh
+    #   ${HOME}/.local/share/chruby/chruby.sh
+    # MAYBE: If you need to cleanup old paths, something like this:
+    if false; then
+      export GEM_PATH="$(\
+        echo ${GEM_PATH} \
+          | /bin/sed -r "s@:?${GEM_HOME}[^:]*:@:@g" \
+          | /bin/sed -r s/^:// \
+          | /bin/sed -r s/:$//
+      )"
+    fi
+    # Check if patch version.
+    PATCH_NUM=$(ruby -e "puts RUBY_VERSION.split('.')[2]")
+    if [[ ${PATCH_NUM} -gt 0 ]]; then
+      # NOTE/2016-12-11: If there's a .ruby-version file in the current
+      #   directory, running *any* command might invoke us, since auto.sh
+      #   sets a trap on DEBUG which runs before every command. Just FYI.
+      # MAYBE: Silence this echo. For now, curious when this fcn. is triggered.
+      echo "Monkey patching!"
+      ruby_set_gem_path
+      # WRONG:
+      #RUBY_ROOT_ZERO=$(echo ${RUBY_ROOT} | /bin/sed -r s/-${RUBY_VERSION}$/-${RUBY_MINOR_ZERO}/)
+      #export PATH="${PATH}:${RUBY_ROOT_ZERO}/bin"
+      #export PATH="${PATH}:${GEM_HOME/${RUBY_VERSION}/${RUBY_MINOR_ZERO}}/bin"
+      local gem_ruby_bin="${GEM_HOME/${RUBY_VERSION}/${RUBY_MINOR_ZERO}}/bin"
+      #if [[ ":${PATH}:" != *":${gem_ruby_bin}:"* ]]; then
+      #  export PATH="${PATH}:${gem_ruby_bin}"
+      #fi
+      path_add_part ${gem_ruby_bin}
+    fi
+  }
+
+  export -f chruby_use
 }
 
 # 2018-09-17: A wrapper I made to support installing same Ruby version
@@ -289,6 +293,12 @@ main () {
   source_deps
 
   ruby_set_gem_path
+
+  if [[ -z ${HOMEFRIES_CHRUBY_SETUP+x} ]]; then
+    patch_export_chruby_use
+# source ruby_chutil.sh
+    export HOMEFRIES_CHRUBY_SETUP=true
+  fi
 }
 
 main "$@"
