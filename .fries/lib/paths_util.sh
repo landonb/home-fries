f832ba3f0cc61e69fd7cae59d67de4f59ad0be43
landonb <landonb@users.noreply.github.com>
2019-09-16 11:51:39 -0500

    Refactor: Split PATH-setting function into many.

--- .fries/lib/paths_util.sh
+++ .fries/lib/paths_util.sh
@@ -22,6 +22,12 @@ source_deps () {
 
 # *** PATH builder commands.
 
+# For the sake of vanity, keep PATH tight and narrow by checking if any path
+# already added and not adding again; or by removing any path first before
+# appending or prefixing it to PATH. (Otherwise, when you reload bash, say,
+# by running /bin/bash from within a terminal, your PATH would otherwise
+# grow with duplicate entries. Which is not harmful, just annoying.)
+
 path_prepend_lazy () {
   # DEAD_PATH/2019-09-16: Included for posterity, but this fcn. not used.
   #                 (lb): I want to show off the ``:${}: != *:${}:*`` trick.
@@ -49,7 +55,8 @@ path_part_remove () {
   fi
   # Substitute: s/:inside:/:/
   PATH=${PATH/:${path_part}:/:}
-  export PATH
+  # The caller should finalize the export::
+  #   export PATH
 }
 
 path_prepend () {
@@ -69,7 +76,7 @@ path_add_part () {
   path_prepend "$1"
 }
 
-path_add_part_append () {
+path_append () {
   local path_part="$1"
   if [[ -d "${path_part}" ]]; then
     path_part_remove "${path_part}"
@@ -85,122 +92,114 @@ path_add_part_append () {
 # So it's not that much of a stretch to include them herein, as
 # opposed to moving to the author's private startup script.
 
-home_fries_set_path_environ () {
-  # 2016-12-06: To avoid making PATH super long -- mostly just an annoyance
-  # if you want to look at, but not a performance issue or anything -- which
-  # happens if you reload your .bashrc by running /bin/bash from a terminal,
-  # collect all the PATH additions and then add them only if not added.
-  local path_prefix=()
-  local path_suffix=()
-
-  # Binary fries.
-  path_prefix+=("${HOMEFRIES_DIR}/bin")
-
-  # 2017-10-03: Make sourcing files easy!
-  path_prefix+=("${HOMEFRIES_DIR}/lib")
-
-  # ~/.local/bin is where, e.g., `pip install --user blah` installs.
-  # And also where zoidy_home-fries installs non-apt/-snap applications.
-  path_prefix+=("${HOME}/.local/bin")
-
-  # Ansible Zoidy Pooh-installed Node/NPM executables.
-  path_prefix+=("${HOME}/.local/node_modules/.bin")
-
-  # Android Studio.
-  export JAVA_HOME=${HOME}/.downloads/jdk
-  export JRE_HOME=${JAVA_HOME}/jre
-  if [[ -d ${JAVA_HOME} ]]; then
-    path_prefix+=("${JAVA_HOME}/bin:${JRE_HOME}/bin")
-  fi
-  if [[ -d ${OPT_BIN}/android-studio/bin ]]; then
-    path_suffix+=("${OPT_BIN}/android-studio/bin")
-  fi
-  if [[ -d ${OPT_BIN}/android-sdk/platform-tools ]]; then
-    path_suffix+=("${OPT_BIN}/android-sdk/platform-tools")
-  fi
-  # 2017-02-25: Have I been missing ANDROID_HOME for this long??
-  export ANDROID_HOME=${HOME}/Android/Sdk
-  if [[ ":${PATH}:" != *":${ANDROID_HOME}/tools:"* ]]; then
-    export PATH=${PATH}:${ANDROID_HOME}/tools
-  fi
-
+home_fries_add_to_path_sbin () {
   # No whep. 2016.04.28 and this is the first time I've seen this.
   #   $ ifconfig
   #   Command 'ifconfig' is available in '/sbin/ifconfig'
   #   The command could not be located because '/sbin' is not included in the PATH environment variable.
   #   This is most likely caused by the lack of administrative privileges associated with your user account.
   #   ifconfig: command not found
-  path_suffix+=("/sbin")
+  path_append "/sbin"
+}
 
-  # 2016-07-11: Google Go, for Google Drive `drive`.
-  #
-  # The latest go binary.
-  if [[ -d /usr/local/go/bin ]]; then
-    # 2018-12-23: Symlinking go from ~/.local/bin, so no sudo needed/not sitewide.
-    # MAYBE/2018-12-23: Remove this PATH part.
-    path_prefix+=("/usr/local/go/bin")
+home_fries_add_to_path_home_fries_bin () {
+  # Make Home Fries commands available.
+  path_prepend "${HOMEFRIES_DIR}/bin"
+}
+
+home_fries_add_to_path_home_fries_lib () {
+  # Make sourcing Home Fries files easy.
+  path_prepend "${HOMEFRIES_DIR}/lib"
+}
+
+home_fries_add_to_path_home_local_bin () {
+  # Make commands installed by Zoidy Pooh et al available.
+  # E.g., ~/.local/bin is where `pip install --user blah` installs.
+  # And where zoidy_home-fries installs non-apt/-snap applications.
+  path_prepend "${HOME}/.local/bin"
+}
+
+home_fries_add_to_path_home_local_node_modules_bin () {
+  # Make Ansible Zoidy Pooh-installed Node/NPM executables available.
+  path_prepend "${HOME}/.local/node_modules/.bin"
+}
+
+home_fries_add_to_path_java_jdk_jre () {
+  local install_dir="${HOME}/.downloads"
+
+  local jdk_dir="${install_dir}/jdk"
+  if [[ -d "${jdk_dir}" ]]; then
+    # FIXME/2019-09-16: Seems like a weird side-effect of updating PATH
+    #                   to also be exporting other variables.
+    export JAVA_HOME="${jdk_dir}"
+    export JRE_HOME="${JAVA_HOME}/jre"
+
+    path_prepend "${JAVA_HOME}/bin"
+    path_prepend "${JRE_HOME}/bin"
   fi
-  if [[ ! -d ${HOME}/.gopath ]]; then
-    # 2016-10-03: Why not?
-    mkdir ${HOME}/.gopath
+}
+
+home_fries_add_to_path_android_studio () {
+  local install_dir="${HOME}/.downloads"
+
+  path_prepend "${install_dir}/android-studio/bin"
+  path_prepend "${install_dir}/android-sdk/platform-tools"
+
+  # 2017-02-25: Have I been missing ANDROID_HOME for this long??
+  local sdk_dir="${install_dir}/Android/Sdk"
+  if [[ -d "${sdk_dir}" ]]; then
+    # FIXME/2019-09-16: Seems like a weird side-effect of updating PATH
+    #                   to also be exporting other variables.
+    export ANDROID_HOME=${install_dir}/Android/Sdk
+    path_append "${ANDROID_HOME}/tools"
   fi
-  if [[ -d ${HOME}/.gopath ]]; then
-    # Local go projects you install.
-    export GOPATH=${HOME}/.gopath
-    # Check with: `go env`
+}
 
-    path_prefix+=("${GOPATH}:${GOPATH}/bin")
+home_fries_add_to_path_golang () {
+  local go_dir="${HOME}/.gopath"
+  if [[ -d "${go_dir}" ]]; then
+    # FIXME/2019-09-16: Seems like a weird side-effect of updating PATH
+    #                   to also be exporting other variables.
+    export GOPATH="${go_dir}"
+    # You can check GOPATH with: `go env`.
+
+    path_prepend "${GOPATH}/bin"
+    path_prepend "${GOPATH}"
   fi
+}
 
-  # OpenShift Origin server.
-  if [[ -d ${OPT_BIN}/openshift-origin-server ]]; then
-    path_suffix+=("${OPT_BIN}/openshift-origin-server")
+home_fries_add_to_path_ruby_version_manager () {
+  # 2017-04-27: Set PATH code added to .bashrc if you run script at https://get.rvm.io.
+  path_append "${HOME}/.rvm/bin"
+}
 
-    # OpenShift development.
-    #  https://github.com/openshift/origin/blob/master/CONTRIBUTING.adoc#develop-locally-on-your-host
-    # Used in one place:
-    #  /exo/clients/openshift/origin/hack/common.sh
-    export OS_OUTPUT_GOPATH=1
-  fi
+home_fries_set_path_environ () {
+  home_fries_add_to_path_sbin
+  unset -f home_fries_add_to_path_sbin
 
+  home_fries_add_to_path_home_fries_bin
+  unset -f home_fries_add_to_path_home_fries_bin
 
-  # 2017-04-27: Added by Bash script at https://get.rvm.io:
-  #   "Add RVM to PATH for scripting. Make sure this is the last PATH variable change."
-  if [[ -d ${HOME}/.rvm/bin ]]; then
-    path_suffix+=("${HOME}/.rvm/bin")
-  fi
+  home_fries_add_to_path_home_fries_lib
+  unset -f home_fries_add_to_path_home_fries_lib
 
-  # ============================
-  # Cleanup PATH before export
-  # ============================
+  home_fries_add_to_path_home_local_bin
+  unset -f home_fries_add_to_path_home_local_bin
 
-  # 2016-12-06: Check if directory in PATH or not (so PATH doesn't
-  # just become really long if you run /bin/bash from a shell).
-  #   https://stackoverflow.com/questions/1396066/
-  #     detect-if-users-path-has-a-specific-directory-in-it
-  #   "Using grep is overkill, and can cause trouble if you're searching for
-  #   anything that happens to include RE metacharacters. This problem can be
-  #   solved perfectly well with bash's builtin [[ command:" [... see below.]
+  home_fries_add_to_path_home_local_node_modules_bin
+  unset -f home_fries_add_to_path_home_local_node_modules_bin
 
-  local path_elem=""
+  home_fries_add_to_path_java_jdk_jre
+  unset -f home_fries_add_to_path_java_jdk_jre
 
-  for ((i = 0; i < ${#path_prefix[@]}; i++)); do
-    path_elem="${path_prefix[$i]}"
-    # Similar to:
-    #  path_add_part "${path_elem}"
-    if [[ ":${PATH}:" != *":${path_elem}:"* ]]; then
-      PATH="${path_elem}:${PATH}"
-    fi
-  done
+  home_fries_add_to_path_android_studio
+  unset -f home_fries_add_to_path_android_studio
 
-  for ((i = 0; i < ${#path_suffix[@]}; i++)); do
-    path_elem="${path_suffix[$i]}"
-    if [[ ":${PATH}:" != *":${path_elem}:"* ]]; then
-      PATH="${PATH}:${path_elem}"
-    fi
-  done
+  home_fries_add_to_path_golang
+  unset -f home_fries_add_to_path_golang
 
-  export PATH
+  home_fries_add_to_path_ruby_version_manager
+  unset -f home_fries_add_to_path_ruby_version_manager
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
