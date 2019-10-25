1be9a92464b790233d90f39a2259418e9cb9ff16
landonb <landonb@users.noreply.github.com>
2019-10-25 15:49:56 -0500

    UX: Tweak progress message lines.

--- .fries/myrepos/lib/git-auto-commit.sh
+++ .fries/myrepos/lib/git-auto-commit.sh
@@ -32,7 +32,7 @@ git_auto_commit_noop () {
 
 git_auto_commit_one () {
   local repo_file="$1"
-  local commit_msg="${2:-Auto-commit ${repo_file} [@$(hostname)].}"
+  local commit_msg="${2:-Commit “${repo_file}” [@$(hostname)] using myrepos.}"
   local extcd
   git_auto_commit_hello
   (git status --porcelain "${repo_file}" |
@@ -71,7 +71,7 @@ git_auto_commit_one () {
 }
 
 git_auto_commit_all () {
-  local commit_msg="${1:-Auto-commit *all* objects with myrepos [@$(hostname)].}"
+  local commit_msg="${1:-Commit *all* dirty files [@$(hostname)] using myrepos.}"
   local extcd
   git_auto_commit_hello
   #
@@ -125,7 +125,7 @@ git_auto_commit_all () {
 }
 
 git_auto_commit_new () {
-  local commit_msg="${1:-Auto-add *untracked* files via myrepos [@$(hostname)].}"
+  local commit_msg="${1:-Commit *new* untracked files [@$(hostname)] using myrepos.}"
   local extcd
   git_auto_commit_hello
   (git status --porcelain . | grep "^[\?][\?]" >/dev/null 2>&1) || extcd=$?
