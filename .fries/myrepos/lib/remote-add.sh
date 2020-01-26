1f3ee444f8f8ef3255e1b4390d34af27ec33b92c
landonb <landonb@users.noreply.github.com>
2020-01-25 21:34:54 -0600

    Feature: New remote_add command for myrepos.

---
+++ .fries/myrepos/lib/remote-add.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# vim:tw=0:ts=2:sw=2:et:norl:nospell:ft=sh
+
+source_deps () {
+# FIXME/2019-10-26 03:20: Improve sourcing...
+  # Load: warn, etc.
+  . ${HOME}/.fries/lib/logger.sh
+}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+remote_add () {
+  local remote_name="$1"
+  local remote_url="$2"
+
+  git remote remove "$1" || true
+  git remote add "$1" "$2"
+}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+main () {
+  source_deps
+}
+
+main "$@"
+
