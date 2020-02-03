b54c9e41bb896615d41d708082ec089541a8bfc9
landonb <landonb@users.noreply.github.com>
2020-02-03 17:47:32 -0600

    Improve: Discard stderr on failable git-remote-remove.

--- .fries/myrepos/lib/remote-add.sh
+++ .fries/myrepos/lib/remote-add.sh
@@ -13,7 +13,7 @@ remote_add () {
   local remote_name="$1"
   local remote_url="$2"
 
-  git remote remove "$1" || true
+  git remote remove "$1" 2> /dev/null || true
   git remote add "$1" "$2"
 }
 
