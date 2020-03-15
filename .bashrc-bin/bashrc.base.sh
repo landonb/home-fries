40714501949e4094bf829a390051e5521fcc3b4e
landonb <landonb@users.noreply.github.com>
2020-03-15 02:29:11 -0500

    Linting: Cleanup comments, quoting, etc.
    - Make more POSIX-friendly: convert [[ ]] to [ ].
    - Linting: Prefer brace-surrounded ${VAR}s.
    - Linting: Prefer `.` vs. `source`.
    - Linting: Use double-quotes around path values.
    - Refactor: Split source-private scripts function,
      and use guard clause.

--- .bashrc-bin/bashrc.base.sh
+++ .bashrc-bin/bashrc.base.sh
@@ -1,10 +1,9 @@
 # File: bashrc.base.sh
 # Author: Landon Bouma (landonb &#x40; retrosoft &#x2E; com)
-# Last Modified: 2017.10.04
-# Project Page: https://github.com/landonb/home-fries
+# Project: https://github.com/landonb/home-fries
 # Summary: Smart Bash Startup Script
-# License: GPLv3
-# vim:tw=0:ts=2:sw=2:et:norl:
+# License: MIT
+# vim:tw=0:ts=2:sw=2:et:norl:ft=sh
 
 # Overview
 # ========
@@ -26,8 +25,10 @@
 # Do Nothing Unless Interactive
 # =============================
 
-# Copied from /etc/bash.bashrc [Ubuntu 18.04]:
+# Ref: Copied from /etc/bash.bashrc [Ubuntu 18.04].
+#  "If not running interactively, don't do anything"
 [ -z "$PS1" ] && return
+
 # (One could also check [[ $- != *i* ]],
 # but not $(shopt login_shell), which is
 # false via mate-terminal.
@@ -37,18 +38,16 @@
 
 bashrc_time_0=$(date +%s.%N)
 
+# YOU: Uncomment to enable logging to stdout:
+#  export DUBS_TRACE=${DUBS_TRACE:-true}
 export DUBS_TRACE=${DUBS_TRACE:-false}
-#export DUBS_TRACE=${DUBS_TRACE:-true}
 
-# DEVS: Uncomment to show progress times.
+# YOU: Uncomment to show progress times.
+#  DUBS_PROFILING=${DUBS_PROFILING:-true}
 DUBS_PROFILING=${DUBS_PROFILING:-false}
-#DUBS_PROFILING=${DUBS_PROFILING:-true}
-[[ -n "${TMUX}" ]] && DUBS_PROFILING=true
+[ -n "${TMUX}" ] && DUBS_PROFILING=true
 
-# DEVS: Uncomment to show progress times.
-DUBS_PROFILING=${DUBS_PROFILING:-false}
-
-$DUBS_TRACE && echo "User's EUID is $EUID"
+${DUBS_TRACE} && echo "User's EUID is ${EUID}"
 
 # Get the path to this script's parent directory.
 # Doesn't work?!:
@@ -56,7 +55,7 @@ $DUBS_TRACE && echo "User's EUID is $EUID"
 # Carnally related:
 #   hard_path=$(dirname $(readlink -f ~/.bashrc))
 # Universally Bashy:
-hard_path=$(dirname $(readlink -f -- "${BASH_SOURCE}"))
+hard_path="$(dirname $(readlink -f -- "${BASH_SOURCE}"))"
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
@@ -68,9 +67,8 @@ print_elapsed_time () {
   local detail="$2"
   local prefix="${3:-Elapsed: }"
   local time_n=$(date +%s.%N)
-  # local elapsed_fract_mins="$(echo "(${time_n} - ${time_0}) / 60" | bc -l)"
   local elapsed_fract_secs="$(echo "(${time_n} - ${time_0})" | bc -l)"
-  if [[ $(echo "${elapsed_fract_secs} >= 0.05" | bc -l) -eq 1 ]]; then
+  if [ $(echo "${elapsed_fract_secs} >= 0.05" | bc -l) -eq 1 ]; then
     local elapsed_secs=$(echo ${elapsed_fract_secs} | xargs printf "%.2f")
     echo "${prefix}${elapsed_secs} secs. / ${detail}"
   fi
@@ -82,11 +80,11 @@ ensure_pathed () {
   # HACK!
   # - Unset BASH_VERSION so ~/.profile doesn't load *us*!
   #   But updates PATH and LD_LIBRARY_PATH instead.
-  local wasver
-  wasver="${BASH_VERSION}"
+  local was_version
+  was_version="${BASH_VERSION}"
   BASH_VERSION=""
-  source ${HOME}/.profile
-  BASH_VERSION="${wasver}"
+  . "${HOME}/.profile"
+  BASH_VERSION="${was_version}"
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
@@ -94,15 +92,15 @@ ensure_pathed () {
 # System-wide Profile
 # ===================
 
-source_deps () {
+source_system_rc () {
   local time_0=$(date +%s.%N)
 
   # Source global definitions.
-  if [[ -f "/etc/bashrc" ]]; then
+  if [ -f "/etc/bashrc" ]; then
     # Fedora.
     . /etc/bashrc
     print_elapsed_time "${time_0}" "Source: /etc/bashrc"
-  elif [[ -f "/etc/bash.bashrc" ]]; then
+  elif [ -f "/etc/bash.bashrc" ]; then
     # Debian/Ubuntu.
     . /etc/bash.bashrc
     print_elapsed_time "${time_0}" "Source: /etc/bash.bashrc"
@@ -119,7 +117,7 @@ source_fries () {
 
   # Load the basic script. Defines aliases, configures things,
   # adjusts the terminal prompt, and adds a few functions.
-  source ${hard_path}/bashrc.core.sh
+  . "${hard_path}/bashrc.core.sh"
 
   print_elapsed_time "${time_0}" "Source: bashrc.core.sh" "==FRIES: "
 }
@@ -129,56 +127,43 @@ source_fries () {
 # Machine-specific Profiles
 # =========================
 
+source_privately () {
+  local srcfile="$1"
+  local srctype="$2"
+  if [ -f "${srcfile}" ]; then
+    ${DUBS_TRACE} && echo "Loading ${srctype} resource script: ${srcfile}"
+    local time_0=$(date +%s.%N)
+    . "${srcfile}"
+    print_elapsed_time "${time_0}" "Source: ${srcfile}"
+  else
+    ${DUBS_TRACE} && echo "Did not find a ${srctype} resource: ${srcfile}"
+  fi
+}
+
+source_private_scripts () {
+  # If present, local a private (uncommitted; symlinked?) bash profile script.
+  local privsrc="${hard_path}/bashrx.private.sh"
+  source_privately "${privsrc}" "private"
+
+  # If present, load a machine-specific script.
+  local privhost="${hard_path}/bashrx.private.$(hostname).sh"
+  source_privately "${privhost}" "host-specific"
+
+  # If present, load a user-specific script.
+  local privuser="${hard_path}/bashrx.private.${LOGNAME}.sh"
+  source_privately "${privuser}" "user-specific"
+}
+
 source_private () {
   # Load the machine-specific scripts first so their exports are visible.
-
-  if [[ $EUID -ne 0 ]]; then
-    $DUBS_TRACE && echo "User is not root"
-
-    # Load a private, uncommitted bash profile script, maybe.
-
-    # Rather than assuming we're in the user's home, e.g.,
-    #  if [[ -f "./somefile" ]] ...
-    # use the `echo` trick:
-    if [[ -f `echo ${hard_path}/bashrx.private.sh` ]]; then
-      $DUBS_TRACE && echo "Loading private resource script: bashrx.private.sh"
-      local time_0=$(date +%s.%N)
-      source ${hard_path}/bashrx.private.sh
-      print_elapsed_time "${time_0}" "Source: ${hard_path}/bashrx.private.sh"
-    fi
-
-    # Load a machine-specific, private, uncommitted script, maybe.
-
-    # Note that $HOSTNAME is set by Bash, and hostname is an executable.
-    # My understanding is that $HOSTNAME won't change if the user changes
-    # the hostname, which doesn't matter here, because $HOSTNAME will be
-    # freshly set. But I think my convention lately is $() over ${}, and,
-    # if anything, by calling the command, I don't need to type upcase.
-    machfile=`echo ${hard_path}/bashrx.private.$(hostname).sh`
-
-    if [[ -f "${machfile}" ]]; then
-      $DUBS_TRACE && echo "Loading machine-specific resource script: ${machfile}"
-      local time_0=$(date +%s.%N)
-      source "${machfile}"
-      print_elapsed_time "${time_0}" "Source: ${machfile}"
-    else
-      $DUBS_TRACE && echo "Did not find a machine-specific resource: ${machfile}"
-    fi
-
-    userfile=`echo ${hard_path}/bashrx.private.${LOGNAME}.sh`
-
-    if [[ -f "${userfile}" ]]; then
-      $DUBS_TRACE && echo "Loading user-specific resource script: ${userfile}"
-      local time_0=$(date +%s.%N)
-      source "${userfile}"
-      print_elapsed_time "${time_0}" "Source: $(basename ${userfile})"
-    else
-      $DUBS_TRACE && echo "Did not find a user-specific resource: ${userfile}"
-    fi
-  else
+  if [ ${EUID} -eq 0 ]; then
     # If the user is root, we'll just load the core script, and nothing fancy.
-    $DUBS_TRACE && echo "User is root"
+    ${DUBS_TRACE} && echo "User is root"
+    return
   fi
+
+  ${DUBS_TRACE} && echo "User is not root"
+  source_private_scripts
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
@@ -187,6 +172,9 @@ source_private () {
 # ==========================================================
 
 start_somewhere_something () {
+  # Unless root, then boot.
+  [ ${EUID} -eq 0 ] && return
+
   # See the script:
   #
   #   ~/.homefries/bin/termdub.py
@@ -198,33 +186,31 @@ start_somewhere_something () {
   #   DUBS_STARTUP  -- Some command to run.
   #   DUBS_TERMNAME -- Title of the terminal window.
 
-  if [[ $EUID -ne 0 ]]; then
-    # Start out in the preferred development directory.
-    if [[ -n "$DUBS_STARTIN" ]]; then
-      cd $DUBS_STARTIN
-    elif [[ -d "$DUBS_STARTIN_DEFAULT" ]]; then
-      cd $DUBS_STARTIN_DEFAULT
-    fi
-    # See: ${hard_path}/.homefries/bin/openterms.sh for usage.
-    if [[ -n "$DUBS_STARTUP" ]]; then
-      # Add the command we're about to execute to the command history (so if the
-      # user Ctrl-C's the process, then can easily re-execute it).
-      # See also: history -c, which clears the history.
-      history -s $DUBS_STARTUP
-      # Run the command.
-      # FIXME: Does this hang the startup script? I.e., we're running the command
-      #        from this script... so this better be the last command we run!
-      #$DUBS_STARTUP
-      local time_0=$(date +%s.%N)
-      eval "${DUBS_STARTUP}"
-      print_elapsed_time "${time_0}" "eval: DUBS_STARTUP"
-    fi
-
-    # The variables have served us well; now whack 'em.
-    export DUBS_STARTIN=''
-    export DUBS_STARTUP=''
-    export DUBS_TERMNAME=''
+  # Start out in the preferred development directory.
+  if [ -n "${DUBS_STARTIN}" ]; then
+    cd "${DUBS_STARTIN}"
+  elif [ -d "${DUBS_STARTIN_DEFAULT}" ]; then
+    cd "${DUBS_STARTIN_DEFAULT}"
   fi
+
+  # See: ${hard_path}/.homefries/bin/openterms.sh for usage.
+  if [ -n "${DUBS_STARTUP}" ]; then
+    # Add the command we're about to execute to the command history (so if the
+    # user Ctrl-C's the process, then can easily re-execute it).
+    # See also: history -c, which clears the history.
+    history -s "${DUBS_STARTUP}"
+    # Run the command.
+    # FIXME: Does this hang the startup script? I.e., we're running the command
+    #        from this script... so this better be the last command we run!
+    local time_0=$(date +%s.%N)
+    eval "${DUBS_STARTUP}"
+    print_elapsed_time "${time_0}" "eval: DUBS_STARTUP"
+  fi
+
+  # The variables have served us well; now whack 'em.
+  export DUBS_STARTIN=''
+  export DUBS_STARTUP=''
+  export DUBS_TERMNAME=''
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
@@ -264,7 +250,8 @@ home_fries_bashrc_cleanup () {
       echo "${bashrc_time_n} - ${bashrc_time_0}" | bc -l | xargs printf "%.2f" \
     )
 
-    source 'logger.sh'
+    # NOTE: Startup scripts will have wired PATH so logger will be found.
+    . logger.sh
     local old_level=${LOG_LEVEL}
     export LOG_LEVEL=${LOG_LEVEL_NOTICE}
     notice "home-fries start-up: ${time_elapsed} secs."
@@ -282,15 +269,15 @@ home_fries_bashrc_cleanup () {
   #   $0 == '-bash'
   local custom_bash
   custom_bash=false
-  if [[ "$0" == 'bash' || "$0" == '-bash' ]]; then
+  if [ "$0" = 'bash' ] || [ "$0" = '-bash' ]; then
     if $(alias bash &> /dev/null); then
-      if [[ $(readlink -f "$(alias bash | /bin/sed -E 's/^.* ([^ ]+\/bash\>).*/\1/')") != '/bin/bash' ]]; then
+      if [ "$(readlink -f "$(alias bash | /bin/sed -E 's/^.* ([^ ]+\/bash\>).*/\1/')")" != '/bin/bash' ]; then
         custom_bash=true
       fi
-    elif [[ $(readlink -f "$(command -v bash)") != '/bin/bash' ]]; then
+    elif [ "$(readlink -f "$(command -v bash)")" != '/bin/bash' ]; then
       custom_bash=true
     fi
-  elif [[ $(readlink -f "$0") != '/bin/bash' ]]; then
+  elif [ "$(readlink -f "$0")" != '/bin/bash' ]; then
     custom_bash=true
   fi
   if ${custom_bash}; then
@@ -300,11 +287,6 @@ home_fries_bashrc_cleanup () {
 
   print_elapsed_time "${time_0}" "cleanup"
 }
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
-
-# Prevent creationix/nvm/install.sh from appending this file.
-# Just be sure that its grep commands find "/nvm.sh" and "$NVM_DIR/bash_completion" [so meta].
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
@@ -320,8 +302,8 @@ main () {
     return  # Will have run switch-client and user will be on another session.
   fi
 
-  source_deps
-  unset -f source_deps
+  source_system_rc
+  unset -f source_system_rc
 
   # FIXME/2018-04-04: This is a hack until I figure out something better.
   # - It exports an environment variable I need in source_fries.
