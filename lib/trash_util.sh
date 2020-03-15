1ebd0a94d06a9e5cfc8dd9f3e26f2949843d6670
landonb <landonb@users.noreply.github.com>
2020-03-15 05:31:58 -0500

    Refactor: Moved trash functions to new project; except the aliases.

---
+++ lib/trash_util.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# vim:tw=0:ts=2:sw=2:et:norl:ft=sh
+# Project: https://github.com/landonb/home-fries
+# License: MIT
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+check_deps () {
+  check_dep 'rm_safe'
+  check_dep 'rm_rotate'
+}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# Fix `rm` to be a respectable trashcan.
+home_fries_create_aliases_trash () {
+  DUBS_USE_TRASH_DIR="${DUBS_USE_TRASH_DIR:-${HOME}}"
+  alias rm='rm_safe'
+  alias rmtrash='rm_rotate'
+}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+main () {
+  check_deps
+  unset -f check_deps
+}
+
+main "$@"
+unset -f main
+
