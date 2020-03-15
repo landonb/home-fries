d20bfc077c78163d01f2ac8d6eac835cae72df61
landonb <landonb@users.noreply.github.com>
2020-03-15 13:42:48 -0500

    Improve: Lazy-load virtualenv_wrapper.sh on first call to workon.
    - With a little pull-the-rug-from-underneath you
      `unset -f` magic in a temporary pre-monkey
      patch `workon` ghost function.

--- .bashrc-bin/bashrc.core.sh
+++ .bashrc-bin/bashrc.core.sh
@@ -144,6 +144,7 @@ source_utils_all () {
   # Earlier: "term_util.sh"
   source_it "time_util.sh"
   source_it "trash_util.sh"
+  source_it "venvwrap_wrap.sh"
 }
 
 # ***
@@ -159,48 +160,6 @@ source_utils () {
     "Sourced ${SOURCE_CNT} files (source_utils)." \
     "SOURCES: "
   unset SOURCE_CNT
-}
-
-# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
-
-source_virtualenvwrapper () {
-  local time_outer_0=$(date +%s.%N)
-
-  # Before sourcing the virturalenv wrapper, remove remnants from PATH.
-  # That is, if you `workon <venv>` and than run `bash` or `tmux`, the
-  # venv binaries will still be on PATH. Then, when sourcing the wrapper
-  # on bash (home-fries) startup, you'd see:
-  #
-  #   /home/<user>/.virtualenvs/<venv>/bin/python:
-  #     Error while finding module specification for 'virtualenvwrapper.hook_loader'
-  #       (ModuleNotFoundError: No module named 'virtualenvwrapper')
-  #   virtualenvwrapper.sh: There was a problem running the initialization hooks.
-  #
-  #   If Python could not import the module virtualenvwrapper.hook_loader,
-  #     check that virtualenvwrapper has been installed for
-  #       VIRTUALENVWRAPPER_PYTHON=/home/landonb/.virtualenvs/dob37/bin/python
-  #         and that PATH is set properly.
-  #
-  # Note the wrapper checks $WORKON_HOME, which defaults to ".virtualenvs"
-  # if blank, to form the path, e.g., workon_home_dir="$HOME/$WORKON_HOME".
-  # - Here we convert PATH separators to newlines;
-  # - ignore any line starting with WORKON; and
-  # - reform the path, converting newlines back to colons.
-  # Note, too, the inner-$, in <$'\n'>, is ANSI-C Quoting.
-  # https://www.gnu.org/software/bash/manual/html_node/ANSI_002dC-Quoting.html
-  PATH="$(echo "${PATH//:/$'\n'}" | grep -v -e "^${HOME}/.virtualenvs/" | tr '\n' ':')"
-  export PATH
-
-  if [ -f "${HOME}/.local/bin/virtualenvwrapper.sh" ]; then
-    . "${HOME}/.local/bin/virtualenvwrapper.sh"
-  elif [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
-    . /usr/local/bin/virtualenvwrapper.sh
-  fi
-
-  print_elapsed_time \
-    "${time_outer_0}" \
-    "Sourced virtualenvwrapper.sh." \
-    "Sourced: "
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
@@ -485,9 +444,6 @@ main () {
   unset -f source_it
   unset -f source_from_user_path_or_homefries_lib
   unset -f source_utils
-
-  source_virtualenvwrapper
-  unset -f source_virtualenvwrapper
 
   home_fries_up
   unset -f home_fries_up
---
+++ lib/venvwrap_wrap.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+# vim:tw=0:ts=2:sw=2:et:norl:ft=sh
+# Project: https://github.com/landonb/home-fries#🍟
+# License: MIT
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# 2020-03-15: Deferred-load virtualenvwrapper.sh, 'cause it takes ~0.15 secs.
+# - That is, I just day retooling Bashrc, might as well make it zippy, too.
+# - Wow, the first time I ran this function, says it took ~1.04 secs. to
+#   source the wrapper, so slow! And supports reasoning this wrapper should
+#   be lazily sourced. (Second time I ran it: 0.55 secs. Pretty wide range,
+#   but consistently slow!)
+
+source_virtualenvwrapper () {
+  local time_outer_0=$(date +%s.%N)
+
+  # Before sourcing the virturalenv wrapper, remove remnants from PATH.
+  # That is, if you `workon <venv>` and than run `bash` or `tmux`, the
+  # venv binaries will still be on PATH. Then, when sourcing the wrapper
+  # on bash (home-fries) startup, you'd see:
+  #
+  #   /home/<user>/.virtualenvs/<venv>/bin/python:
+  #     Error while finding module specification for 'virtualenvwrapper.hook_loader'
+  #       (ModuleNotFoundError: No module named 'virtualenvwrapper')
+  #   virtualenvwrapper.sh: There was a problem running the initialization hooks.
+  #
+  #   If Python could not import the module virtualenvwrapper.hook_loader,
+  #     check that virtualenvwrapper has been installed for
+  #       VIRTUALENVWRAPPER_PYTHON=/home/landonb/.virtualenvs/dob37/bin/python
+  #         and that PATH is set properly.
+  #
+  # Note the wrapper checks $WORKON_HOME, which defaults to ".virtualenvs"
+  # if blank, to form the path, e.g., workon_home_dir="$HOME/$WORKON_HOME".
+  # - Here we convert PATH separators to newlines;
+  # - ignore any line starting with WORKON; and
+  # - reform the path, converting newlines back to colons.
+  # Note, too, the inner-$, in <$'\n'>, is ANSI-C Quoting.
+  # https://www.gnu.org/software/bash/manual/html_node/ANSI_002dC-Quoting.html
+  PATH="$(echo "${PATH//:/$'\n'}" | grep -v -e "^${HOME}/.virtualenvs/" | tr '\n' ':')"
+  export PATH
+
+  if [ -f "${HOME}/.local/bin/virtualenvwrapper.sh" ]; then
+    . "${HOME}/.local/bin/virtualenvwrapper.sh"
+  elif [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
+    . /usr/local/bin/virtualenvwrapper.sh
+  fi
+
+  # You: Remove DUBS_PROFILING=false (or set =true) to confirm that
+  # sourcing virtualenvwrapper takes a noticeable amount of time.
+  DUBS_PROFILING=false print_elapsed_time \
+    "${time_outer_0}" \
+    "Sourced virtualenvwrapper.sh." \
+    "Sourced: "
+}
+
+workon () {
+  unset -f workon
+
+  source_virtualenvwrapper
+  unset -f source_virtualenvwrapper
+
+  workon "$@"
+}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+main () {
+  :
+}
+
+main "$@"
+
