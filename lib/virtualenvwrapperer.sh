9255fbda871592edd97a641f62640490a0a5b254
landonb <landonb@users.noreply.github.com>
2020-09-26 16:05:44 -0500

    Linting: Enclose paths in quotes.

--- lib/virtualenvwrapperer.sh
+++ lib/virtualenvwrapperer.sh
@@ -43,8 +43,8 @@ source_virtualenvwrapper () {
 
   if [ -f "${HOME}/.local/bin/virtualenvwrapper.sh" ]; then
     . "${HOME}/.local/bin/virtualenvwrapper.sh"
-  elif [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
-    . /usr/local/bin/virtualenvwrapper.sh
+  elif [ -f "/usr/local/bin/virtualenvwrapper.sh" ]; then
+    . "/usr/local/bin/virtualenvwrapper.sh"
   fi
 
   # You: Remove HOMEFRIES_PROFILING=false (or set =true) to confirm that
