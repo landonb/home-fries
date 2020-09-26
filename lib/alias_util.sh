43033282ec5c9d4516e2d17eb1fd21edbcb67d1c
landonb <landonb@users.noreply.github.com>
2020-09-26 13:13:21 -0500

    Linting: Use POSIX-friendly single square brackets.

--- lib/alias_util.sh
+++ lib/alias_util.sh
@@ -98,7 +98,7 @@ home_fries_create_aliases_general () {
   # Vi vs. Vim: When logged on as root, vi is a dumbed-down vim. Root rarely
   # needs vanilla `vi` -- only when the home directories aren't mounted -- so
   # we can alias vi to vim.
-  if [[ $EUID -eq 0 ]]; then
+  if [ $EUID -eq 0 ]; then
     alias vi="vim"
   fi
   # 2019-03-26: Avoid errors when vim.tiny tries to load your ~/.vim! E.g.,
@@ -392,7 +392,7 @@ home_fries_create_aliases_grep_and_egrep () {
   #   -n, --line-number
   #   -R, --dereference-recursive
   #   -i, --ignore-case
-  if [[ -e $HOME/.grepignore ]]; then
+  if [ -e "$HOME/.grepignore" ]; then
     alias eg='egrep -n -R -i --color --exclude-from="$HOME/.grepignore"'
     alias egi='egrep -n -R --color --exclude-from="$HOME/.grepignore"'
   fi
