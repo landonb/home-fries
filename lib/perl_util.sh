4e1c181b2003b4278fc3f45687ca9ed96a80f5d8
landonb <landonb@users.noreply.github.com>
2020-11-17 22:34:50 -0600

    Tweak: Cleanup Perl REPL prompt.
    - I'm probably overthinking this.

--- lib/perl_util.sh
+++ lib/perl_util.sh
@@ -40,7 +40,57 @@ perl_set_path_and_environs () {
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
-alias perl-repl='rlwrap perl -d -e 1'
+# Perl REPL.
+
+# V. Basic REPL:
+#
+#   alias perl-repl='perl -de1'
+#
+# Ref: https://stackoverflow.com/a/73703/14159598
+
+# Use readline wrapper to add history, completion, and line editing:
+#
+#   alias perl-repl='rlwrap perl -d -e 1'
+#
+# (though it appears history and line editing work for me in the
+#  V. Basic REPL, and I do not see completion working via rlwrap,
+#  so in practice this behaves same for me as previous).
+#
+# Ref: https://stackoverflow.com/questions/73667/
+#   how-can-i-start-an-interactive-console-for-perl#comment41519549_73703
+
+# Step it up a notch, cleanup the prompt, from this:
+#
+#   __DB<1>_print "hello, perld"
+#
+# to this:
+#
+#   perl> print "hello, perld"
+#
+# And add line evaluation, similar to Python and Node REPLs, e.g.,
+#
+#   $ perl-repl
+#   perl> 123
+#   123
+#   perl> 123 + 345
+#   468
+#   perl> $q = "hello"
+#   hello
+#
+# But note that a `print` itself is also evaluated, and it
+# returns 1, which is printed after what `print` prints, e.g.,
+#
+#   perl> print "abc"
+#   abc1
+#   perl> print $q
+#   hello1
+#
+# but given the evaluator itself, you won't need to call `print`.
+#
+# Ref: https://stackoverflow.com/a/22840242/14159598
+perl-repl () {
+  rlwrap -A -pgreen -S"perl> " perl -wnE'say eval()//$@'
+}
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
