49dd38b406fd225712b4cbfdea3d7e3831865db7
landonb <landonb@users.noreply.github.com>
2020-11-17 22:55:47 -0600

    Improve: Tweak Perl REPL to print array/hash contents not length.
    - And TMI comments including links to Perl REPL projects
      (none seemingly active though).

--- lib/perl_util.sh
+++ lib/perl_util.sh
@@ -88,9 +88,84 @@ perl_set_path_and_environs () {
 # but given the evaluator itself, you won't need to call `print`.
 #
 # Ref: https://stackoverflow.com/a/22840242/14159598
+#
+#   rlwrap -A -pgreen -S"perl> " perl -wnE'say eval()//$@'
+#
+# Except arrays are not handled, e.g.,:
+#
+#   perl> @ocouple = qw( Felix Oscar );
+#   2
+#   perl> @ocouple
+#   2
+#   perl> scalar @ocouple
+#   2
+
+# Which brings us further down the SO discussion to a comment offering
+# "a nice output when the expression evaluates to a list or a reference".
+# Indeed, e.g.,:
+#
+#   perl> @ocouple = qw( Felix Oscar );
+#   $VAR1 = [
+#             'Felix',
+#             'Oscar'
+#           ];
+#
+#   perl> @ocouple
+#   $VAR1 = [
+#             'Felix',
+#             'Oscar'
+#           ];
+#
+#   perl> scalar @ocouple
+#   $VAR1 = [
+#             2
+#           ];
+#
+# except now *everything* is printed as an array...
+#
+#   perl> 1234
+#   $VAR1 = [
+#             1234
+#           ];
+#
+# nothing's ever perfect, is't.
 perl-repl () {
-  rlwrap -A -pgreen -S"perl> " perl -wnE'say eval()//$@'
+  # Ref: michau 2019-07-14: https://stackoverflow.com/questions/73667/
+  #   how-can-i-start-an-interactive-console-for-perl#comment100588494_22840242
+  rlwrap -A -pgreen -S'perl> ' perl -MData::Dumper -wnE'say Dumper[eval()]//$@'
 }
+
+# MEH/2020-11-17 22:49: Here's one that maybe addresses previous issue?
+# - Requires Data::Printer.
+#     sudo cpan Data::Printer
+# Ref: https://stackoverflow.com/a/31283257/14159598
+#
+# Hint: "Use `p @<arrayOrList>` or `p %<hashTable>` to print
+#        arrays/lists/hashtables; e.g.: `p %ENV`"
+#
+#  alias iperl='rlwrap -A -S "iperl> " perl -MData::Printer -wnE '\'' BEGIN { say "HI"; } say eval()//$@'\'
+
+# Other Perl REPL projects:
+#
+# - perlconsole (4+ years stale)
+#   https://metacpan.org/pod/release/SUKRIA/perlconsole-0.4/perlconsole
+#
+# - Devel::REPL - A modern perl interactive shell (5+ years stale)
+#   https://metacpan.org/pod/Devel::REPL
+#   https://github.com/p5sagit/Devel-REPL
+#   https://metacpan.org/release/Devel-REPL
+#
+# - Reply (4+ years stale)
+#   https://metacpan.org/pod/Reply
+#   https://github.com/doy/reply
+#
+# - perli (2 years stale)
+#   https://github.com/mklement0/perli
+#   https://github.com/mklement0/perli#examples
+#
+# - Perl Shell (psh) (8-21 years old)
+#   https://gregorpurdy.com/psh/
+#   https://github.com/gnp/psh
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
