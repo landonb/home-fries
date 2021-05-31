2523e8c32f4bdf16596646fbd0186827c6511541
landonb <landonb@users.noreply.github.com>
2021-05-31 16:46:10 -0500

    Docs: Refresh comment re: gpg-agent usage.

--- lib/crypt/daemonize_gpg_agent.sh
+++ lib/crypt/daemonize_gpg_agent.sh
@@ -6,42 +6,82 @@
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
-# 2016-10-25: See stage_4_password_store
-# Apparently not always so sticky.
-# E.g., just now,
-#   $ pass blah/blah
-#   gpg: WARNING: The GNOME keyring manager hijacked the GnuPG agent.
-#   gpg: WARNING: GnuPG will not work properly - please configure that tool
-#                 to not interfere with the GnuPG system!
-#   gpg: problem with the agent: Invalid card
-#   gpg: decryption failed: No secret key
-# and then I got the GUI prompt and not the curses prompt.
-# So maybe we should always give this a go.
+# 2021-05-31: The function below starts gpg-agent as a daemon process.
 #
-# 2016-11-01: FIXME: Broken again. I see a bunch of gpg-agents running, but GUI still pops...
-#   Didn't work:
-#    sudo dpkg-divert --local --rename \
-#      --divert /etc/xdg/autostart/gnome-keyring-gpg.desktop-disable \
-#      --add /etc/xdg/autostart/gnome-keyring-gpg.desktop\
-#   Didn't work:
-#     killall gpg-agent
-#     gpg-agent --daemon
-# What happened to pinentry-curses?
-#   Didn't work:
-#     gpg-agent --daemon > /home/landonb/.gnupg/gpg-agent-info-larry
-#     ssh-agent -k
-#     bash
+# - It's not necessary to start gpg-agent explicitly, as running `gpg`
+#   will load it on-demand. But it's nice to get it going anyway if
+#   you're more than likely to use it at some point during your session.
 #
-# 2021-05-31: I tried killall today but it didn't work.
-# - Use either pkill or gpgconf --kill to kill gpg-agent:
-#     pkill gpg-agent
-#     gpgconf --kill gpg-agent
-# - Verify with `ps aux | grep gpg`.
-# - Note that when I restarted gpg-agent, I got a misleading error:
-#     $ gpg-agent --daemon
-#     gpg-agent: a gpg-agent is already running - not starting a new one
-#   though `ps` showed nothing running before, and one running after.
-#   So not sure what's up with the error. But I justed ignored it.
+# - You'll want to configure you ~/.gnupg/gpg-agent.conf appropriately.
+#
+#   - Historically, I had just one entry:
+#
+#       pinentry-program /usr/bin/pinentry-curses
+#
+#     which tells GPG to prompt for your password in the terminal session.
+#
+#     (I had trouble in 2016 with the GNOME keyring manager spewing errors
+#      when I'd run `pass`, and prompting me through the GUI (and not the
+#      ncurses terminal prompt). But after specifying pinentry-program, my
+#      woes went away.)
+#
+#   - But recently, I've added a few more options to `gpg-agent.conf`:
+#
+#       allow-preset-passphrase
+#       default-cache-ttl 1200
+#       max-cache-ttl 34560000
+#
+#     The `allow-preset-passphrase` option enables `gpg-preset-passphrase`,
+#     which I use to cache my Git/GitHub commit signing key, so that I'm
+#     not constantly being prompted for the passphrase when I commit.
+#
+#     And the default maximum cache timeout is 2 hours, so I also jacked up
+#     the `max-cache-ttl` (to a whopping 400 days, 4 times longer than my
+#     machine's current uptime). This means I should only have to enter the
+#     Git signing key passphrase once per session, after a reboot.
+#
+#     And because that's the only key that I preset, I'm not concerned that
+#     it lives so long in cache — I'm not expecting anyone to gain access
+#     to my terminal and then to start committing code surreptitiously. If
+#     someone accesses my terminal, they're not their to push commits.
+#
+#     Finally, I doubled the default cache timeout, from 10 minutes to 20,
+#     just so I can avoid having to enter my passphrase too frequently when
+#     I'm at my terminal and working on tasks that require multiple, but
+#     somewhat infrequent calls to `pass` (where `pass` is really the only
+#     GPG use I have other than Git signing).
+#
+# - If you've edited `gpg-agent.conf`, it's easiest just to restart it:
+#
+#       gpgconf --reload gpg-agent
+#
+#       # Or, alternatively:
+#       $ gpg-connect-agent reloadagent /bye
+#       OK
+#
+#       # Or even:
+#       $ echo RELOADAGENT | gpg-connect-agent > /dev/null
+#       OK
+#
+# - If you want to kill the agent, avoid `killall gpg-agent` (it doesn't
+#   do anything when I use it), and use `gpgconf` or `pkill` instead:
+#
+#       gpgconf --kill gpg-agent
+#
+#       # Or:
+#       pkill gpg-agent
+#
+#   And verify with `ps aux | grep gpg`.
+#
+#   Note that after killing the agent and restarting it, I see a
+#   misleading "error":
+#
+#       $ gpg-agent --daemon
+#       gpg-agent: a gpg-agent is already running - not starting a new one
+#
+#   Which I call "misleading" because a `ps` beforehand shows nothing
+#   running, and another `ps` afterward shows one process running. So
+#   I am not sure what is up with the message. But I just ignored it.
 
 _homefries_ps_check_if_running () {
   local process_name="$1"
