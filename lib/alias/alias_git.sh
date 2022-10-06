a53914f4a6b91afec8047c37136bf763a6779122
landonb <landonb@users.noreply.github.com>
2022-10-05 21:54:30 -0500

    Sync: Align `git diff` commands with git-smart project.

--- lib/alias/alias_git.sh
+++ lib/alias/alias_git.sh
@@ -6,22 +6,36 @@
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
+# Note that some of these aliases duplicate commands from git-smart:
+#   https://github.com/landonb/git-smart
+# So, perhaps, we could instead just call Git, e.g.,
+#   alias dff='git dff'
+#   alias dfd='git dfd'
+#   alias dcc='git dcc'
+#   alias dcd='git dcd'
+# But Homefries and Git Smart are separate projects.
+# - If you want both together, check out DepoXy:
+#   FIXME: Lint to DepoXy, after it's released... ;)
+# SYNC_ME: See also the same git-smart definitions:
+#   git-smart/.gitconfig
 home_fries_aliases_wire_git () {
-  # I run `git diff` fairly often. Make it even easier to run.
-  # 2020-02-13: See also, via git-smart/.gitconfig:
-  #   `git dc`, `git df`, `git dff`.
+  # Aka `git dff`, if you use git-smart.
   alias dff='git diff'
+  alias dfd='git --no-pager diff'
 
-  # 2020-02-17: Will it be confusing to have a similar alias
-  #             ('d'-double-consonant) but without the pager?
-  #  alias dcc='git --no-pager diff --cached'
-  alias dcd='git --no-pager diff --cached'
+  # Aka `git dcc`, if you use git-smart.
   alias dcc='git diff --cached'
+  alias dcd='git --no-pager diff --cached'
+
+  # *** The following are not in git-smart. They're pure Bash shortcuts.
 
   # 2020-12-01: Why not.
   alias gap='git add -p'
-  # 2021-01-25: Weeds.
-  alias gcv='git ci -v'
+
+  # 2021-01-25: Weeds. (Aka, `git ci -v`.)
+  # 2022-10-05: This shows diff at bottom of commit message template.
+  # - But I don't think I've ever used it. (I def. don't remember it.)
+  #  alias gcv='git commit -v'
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
