fa9c4c50448e4c0231b60abda554227c3074718e
landonb <landonb@users.noreply.github.com>
2022-11-04 23:46:00 -0500

    Improve: Add macOS Gimp `gimp` alias.

--- lib/alias/alias_gimp.sh
+++ lib/alias/alias_gimp.sh
@@ -7,29 +7,44 @@
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
 home_fries_aliases_wire_gimp () {
-  home_fries_create_alias_flatpak_gimp
+  home_fries_create_alias_gimp_flatpak
+  home_fries_create_alias_gimp_macos
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
-home_fries_create_alias_flatpak_gimp () {
-  if [ -d "${HOME}/.var/app/org.gimp.GIMP" ]; then
-    # Desktop entry is more complicated:
-    #   /usr/bin/flatpak run \
-    #     --branch=stable \
-    #     --arch=x86_64 \
-    #     --command=gimp-2.10 \
-    #     --file-forwarding org.gimp.GIMP \
-    #     @@u %U @@
-    alias gimp='flatpak run org.gimp.GIMP'
-  fi
+home_fries_create_alias_gimp_flatpak () {
+  [ -d "${HOME}/.var/app/org.gimp.GIMP" ] || return
+
+  # Desktop entry is more complicated:
+  #   /usr/bin/flatpak run \
+  #     --branch=stable \
+  #     --arch=x86_64 \
+  #     --command=gimp-2.10 \
+  #     --file-forwarding org.gimp.GIMP \
+  #     @@u %U @@
+
+  # Note that we shadow /usr/bin/gimp
+  alias gimp="flatpak run org.gimp.GIMP"
+}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+home_fries_create_alias_gimp_macos () {
+  [ "$(uname)" = "Darwin" ] || return
+
+  # gimp is hashed (/usr/local/bin/gimp) by default, but
+  # running `gimp` itself generates warnings and exits 255.
+
+  claim_alias_or_warn "gimp" "open /Applications/GIMP-2.10.app"
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
 unset_f_alias_gimp () {
   unset -f home_fries_aliases_wire_gimp
-  unset -f home_fries_create_alias_flatpak_gimp
+  unset -f home_fries_create_alias_gimp_flatpak
+  unset -f home_fries_create_alias_gimp_macos
   # So meta.
   unset -f unset_f_alias_gimp
 }
