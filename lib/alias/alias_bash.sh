8bfd3ab24da3f22dc21aa5845e5486cc47e4d6e3
landonb <landonb@users.noreply.github.com>
2023-02-02 13:58:08 -0600

    Improve: Use same bash when running bash with bash on bash

--- lib/alias/alias_bash.sh
+++ lib/alias/alias_bash.sh
@@ -9,11 +9,20 @@
 home_fries_aliases_wire_bash () {
   # 2016-06-28: Stay in same dir when launching bash.
   unalias bash 2> /dev/null
+
   # 2020-03-22: If run from tmux, ensure PROMPT_COMMAND unset,
   #             lest on every command you see, e.g.,
   #               __vte_prompt_command: command not found
   #             (AFAIK, __vte_prompt_command added by tmux.)
-  alias bash='HOMEFRIES_CD="$(pwd)" PROMPT_COMMAND= bash'
+  #
+  #  alias bash='HOMEFRIES_CD="$(pwd)" PROMPT_COMMAND= bash'
+
+  # 2023-02-02: To support Homebrew or custom bash, so that, e.g.,
+  # `bash` opens Homebrew Bash v5, and not macOS system bash v3:
+  # - We either need to symlink ~/.local/bin/bash -> $(brew --prefix)/bin/bash
+  #   - Or we could call `$0` herein, instead of `bash`.
+  # - Using $0 feels like the more proper solution.
+  alias bash='HOMEFRIES_CD="$(pwd)" PROMPT_COMMAND= $0'
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
