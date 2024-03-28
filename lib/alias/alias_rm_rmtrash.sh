8c499239b55983ec3960c9013563ade7e804e285
Landon Bouma <landonb@users.noreply.github.com>
2024-03-28 01:25:26 -0500

    Feature: New `rm_dotglob`, to complement the other two

--- lib/alias/alias_rm_rmtrash.sh
+++ lib/alias/alias_rm_rmtrash.sh
@@ -16,9 +16,29 @@ check_deps () {
 # Change `rm` into a respectable trashcan.
 home_fries_aliases_wire_rm_rmtrash () {
   RM_SAFE_TRASH_HOME="${RM_SAFE_TRASH_HOME:-${HOME}}"
+
   # Use full path so `sudo rm` works.
   alias rm="${SHOILERPLATE:-${HOME}/.kit/sh}/sh-rm_safe/bin/rm_safe"
+
   claim_alias_or_warn "rmtrash" "rm_rotate"
+
+  # Remove a glob of files and include .dotted (hidden) files.
+  # - CXREF: See extensive comments re: `alias mv`:
+  #   ~/.homefries/lib/alias/alias_mv.sh
+  claim_alias_or_warn "rm." "set +f ; shopt -s dotglob ; rm_dotglob"
+  claim_alias_or_warn "rm_all" "set +f ; shopt -s dotglob ; rm_dotglob"
+}
+
+# - CXREF: See extensive comments re: `mv_dotglob`:
+#   ~/.homefries/lib/alias/alias_mv.sh
+
+rm_dotglob () {
+  ${SHOILERPLATE:-${HOME}/.kit/sh}/sh-rm_safe/bin/rm_safe "$@"
+
+  # Leave noglob unset (set +f)
+  #  set -f
+
+  shopt -u dotglob
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
