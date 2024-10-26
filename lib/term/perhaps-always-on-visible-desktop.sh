97d4b52114b3272699ba9ef5132554fee1ccbd02
Landon Bouma <landonb@users.noreply.github.com>
2024-10-25 20:45:51 -0500

    Bugfix: Repair sticky mate-terminal feature broken by window number prefix
    - Regression caused by ac6ae74:
    
        *Feature: Add window number to title for Alacritty and mate-terminal*

--- lib/term/perhaps-always-on-visible-desktop.sh
+++ lib/term/perhaps-always-on-visible-desktop.sh
@@ -16,7 +16,7 @@ sleep_then_ensure_always_on_visible_desktop () {
   sleep 3  #  MAGIC_NUMBER: It takes a few seconds for Home Fries to load.
   local winids
   winids=($(wmctrl -l -p \
-    | /bin/grep -E "^0x[a-f0-9]{8} +-?[0-3] +[0-9]+ +$(hostname) +${DUBS_STICKY_PREFIX_RE}" \
+    | /bin/grep -E "^0x[a-f0-9]{8} +-?[0-3] +[0-9]+ +$(hostname) +[0-9]+. ${DUBS_STICKY_PREFIX_RE}" \
     | cut -d ' ' -f 1))
   printf "%s\n" "${winids[@]}" | xargs -I % wmctrl -b add,sticky -i -r %
 }
