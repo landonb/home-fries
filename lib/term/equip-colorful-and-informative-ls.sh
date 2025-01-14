d645e48bdb6e906ecbc3e23efc93926cda377877
Landon Bouma <landonb@users.noreply.github.com>
2025-01-14 11:14:13 -0600

    Update: Change OS suss test from /proc/version to /etc/os-release

--- lib/term/equip-colorful-and-informative-ls.sh
+++ lib/term/equip-colorful-and-informative-ls.sh
@@ -20,11 +20,11 @@ home_fries_fix_terminal_colors () {
   # Use the following commands to generate the export variable on your machine:
   #   dircolors --print-database
   #   dircolors --sh
-  if [ -e /proc/version ]; then
+  if [ -e /etc/os-release ]; then
     # REFER: For ways to suss OS, see comments in distro lib:
     #   ~/.kit/sh/home-fries/lib/distro_util.sh
-    if [ "$(cat /proc/version | grep Ubuntu)" ]; then
-      # echo Ubuntu!
+    if [ "$(cat /etc/os-release | grep -q "^ID=linuxmint\$")" ]; then
+      # OS: Linux Mint
       # EXPLAIN/2020-08-31 19:19: Why not just call `eval $(dircolors)` here?
       LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:su=37;41:sg=30;43:tw=30;47:ow=34;47:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.svgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.lzma=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.dz=01;31:*.gz=01;31:*.bz2=01;31:*.bz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.rar=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:'
     # # ISOFF/2025-01-14: It's been years since this settings has been
