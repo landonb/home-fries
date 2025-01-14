881aabcc8187d68e9b5696b98eb20210d29b1a92
Landon Bouma <landonb@users.noreply.github.com>
2025-01-14 11:32:05 -0600

    Update: Replace /proc/version usage with /etc/os-release
    - Also, replace [[ Bashisms ]] with inline command expression.

--- lib/distro_util.sh
+++ lib/distro_util.sh
@@ -237,12 +237,12 @@ screensaver_lockon () {
 
 suss_apache () {
   # Determine the name of the apache user.
-  if [[ "$(cat /proc/version | grep Ubuntu)" ]]; then
-    # echo Ubuntu.
+  if cat /proc/version | grep -q "Ubuntu"; then
+    # Ubuntu.
     httpd_user=www-data
     httpd_etc_dir=/etc/apache2
-  elif [[ "$(cat /proc/version | grep Red\ Hat)" ]]; then
-    # echo Red Hat.
+  elif cat /proc/version | grep -q "Red Hat"; then
+    # Red Hat.
     httpd_user=apache
     httpd_etc_dir=/etc/httpd
   else
