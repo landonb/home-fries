d1238c2d7bc8f2d988aef05c2d60d83205f853a9
Landon Bouma <landonb@users.noreply.github.com>
2025-02-03 12:44:40 -0600

    Docs: Update paths to relocate resources

--- lib/fries_util.sh
+++ lib/fries_util.sh
@@ -360,8 +360,8 @@ home_fries_punch_anacron () {
 
 # CXREF: Runs vim or nvim (latter if EDITOR_PREFER_NVIM=true).
 # - If running a DepoXy environ, found locally at:
-#   ~/.kit/git/tig-newtons/bin/editor-vim-0-0-insert-minimal
-#   ~/.kit/git/tig-newtons/bin/editor-vim-0-0-insert-minimal.vimrc
+#   ~/.kit/nvim/nvim-depoxy/bin/editor-vim-0-0-insert-minimal
+#   ~/.kit/nvim/nvim-depoxy/bin/editor-vim-0-0-insert-minimal.vimrc
 #   ~/.depoxy/running/home/.config/depoxy/depoxyrc
 
 home_fries_export_editor_vim () {
