1ef2bfb9337ce9d592e6506d9f6e23c772316e8a
Landon Bouma <landonb@users.noreply.github.com>
2025-09-10 02:11:51 -0500

    refactor(lint): reformat by shfmt

--- lib/alias/alias_clip.sh
+++ lib/alias/alias_clip.sh
@@ -9,18 +9,18 @@
 # USAGE: echo "foo" | clip
 #
 # - Copies whatever is on stdin to the clipboard.
-home_fries_aliases_wire_clip () {
+home_fries_aliases_wire_clip() {
   claim_alias_or_warn "clip" "_hf_clip"
 }
 
-_hf_clip () {
+_hf_clip() {
   tee >(tr -d "\n" | $(command -v pbcopy > /dev/null && pbcopy || xclip -selection c))
 }
 
 # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
 
 # Called by `home_fries_bashrc_cleanup`
-unset_f_alias_clip () {
+unset_f_alias_clip() {
   unset -f home_fries_aliases_wire_clip
   # So meta.
   unset -f unset_f_alias_clip
@@ -31,4 +31,3 @@ unset_f_alias_clip () {
 if [ "$0" = "${BASH_SOURCE[0]}" ]; then
   >&2 echo "ERROR: Trying sourcing the file instead: . $0" && exit 1
 fi
-
