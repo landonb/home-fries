e22bef3a4f47bbf90c7081d610b4cd18ac8304e0
Landon Bouma <landonb@users.noreply.github.com>
2025-09-20 03:41:01 -0500

    deps: update faithfuls <sh-humble-prompt>
    - Source: sh-humble-prompt @ 2d23c29f7229 [1.1.0 at HEAD~15 “ac92b07”]
    
    - Commit generated by:
    
        https://github.com/thegittinsgood/git-update-faithful#⛲
    
      under the guidance of:
    
        https://github.com/DepoXy/depoxy/blob/release/home/.kit/git/ohmyrepos/lib/my-deps-manage-shoilerplate.sh

---
+++ deps/sh-humble-prompt/deps/gnome-window-calls/lib/gnome-window-calls.sh
@@ -0,0 +1,339 @@
+# vim:tw=0:ts=2:sw=2:et:norl:ft=bash
+# Author: Landon Bouma <https://tallybark.com/>
+# Project: https://github.com/DepoXy/gnome-window-calls#🪟
+# License: MIT. Please find more in the LICENSE file.
+
+# Copyright (c) © 2024-2025 Landon Bouma. All Rights Reserved.
+
+# USAGE: Wire into your app.
+# - REFER: See DepoXy users:
+#     https://github.com/DepoXy/sh-humble-prompt#🙇
+#     https://github.com/DepoXy/gvim-open-kindness#🐬
+
+# REFER: Depends on `window-calls` GNOME Shell extension:
+#   https://extensions.gnome.org/extension/4724/window-calls/
+#   https://github.com/ickyicky/window-calls
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# DEVEL: Uncomment to capture trace files in the current directory.
+# - The trace files are named with number prefixes, e.g., "01", "02",
+#   etc., so you can easily review using `less 0*` and then :n and :p
+#   to navigate next and previous in less.
+#
+#  RAISELOWER_TRACE_DIR="."
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+get_window_ids_Wayland() {
+  get_window_ids_Wayland_filtered ""
+}
+
+get_window_ids_Wayland_focused() {
+  local jq_filter="select(.focus == true)"
+
+  get_window_ids_Wayland_filtered "${jq_filter}"
+}
+
+get_window_ids_Wayland_filtered() {
+  local jq_filter="$1"
+
+  # Returns JSON blob as embedded string, either like
+  #   ('[{JSON}]',)
+  # or like
+  #   ("[{ESCAPED-JSON}]",)
+  local windows_list_raw
+  if ! windows_list_raw="$(
+    gdbus call --session --dest org.gnome.Shell \
+      --object-path /org/gnome/Shell/Extensions/Windows \
+      --method org.gnome.Shell.Extensions.Windows.List \
+      2> /dev/null
+  )"; then
+    # E.g. — Error: GDBus.Error:org.freedesktop.DBus.Error.UnknownMethod:
+    #   Object does not exist at path “/org/gnome/Shell/Extensions/Foo”
+    #
+    # SAVVY: check_deps should make this path unreachable.
+    alert_missing_gnome_extension_window_calls
+
+    return 1
+  fi
+
+  # USAGE: E.g.,
+  #   (RAISELOWER_TRACE_DIR=/tmp/rltrace && get_window_ids_Wayland)
+  if [ -n "${RAISELOWER_TRACE_DIR}" ]; then
+    mkdir -p -- "${RAISELOWER_TRACE_DIR}"
+
+    echo "${windows_list_raw}" > "${RAISELOWER_TRACE_DIR}/01--windows_list_raw"
+  fi
+
+  # ***
+
+  # Note the window-calls response defaults to a single-
+  # quoted stream of JSON text, where the JSON fields
+  # are double-quoted.
+  # - But if the window title is named with a single-
+  #   quote, then the stream of JSON text is double-
+  #   quoted instead, and thusly the field quotes
+  #   need to be escaped.
+  #
+  # - When double-quoted, the response looks like this:
+  #     ('[{"in_current_workspace":true,..."focus":true}]',)
+  # - And if a value within contains double quotes, they
+  #   themselves will be double-escaped, e.g., a webpage
+  #   titled `air "quotes"` might look like this:
+  #     ..."title":"air \\"quotes\\" - Google Chrome",...
+  #
+  # - But if a value contains a single quote, then the whole
+  #   blob is double-quoted instead, e.g.,
+  #     ("[{\"in_current_workspace\":true,...\"focus\":true}]",)
+  # - And if a value within contains double quotes, they're
+  #   obviously additionally escaped, e.g.,
+  #     ...\"title\":\"\\\"destiny's child\\\" - Google Chrome\",...
+  #
+  # To avoid jq issues, we'll remove escaped quotes within
+  # values first — then we'll unescape escaped field quotes.
+
+  test_double_quoted() { echo "$1" | grep -q -e '^("'; }
+
+  test_single_quoted() { echo "$1" | grep -q -e "^('"; }
+
+  local is_double_quoted=false
+  local is_single_quoted=false
+  if test_double_quoted "${windows_list_raw}"; then
+    is_double_quoted=true
+  elif test_single_quoted "${windows_list_raw}"; then
+    is_single_quoted=true
+  else
+    >&2 "ERROR: Unrecognized D-Bus response format: Not quoted?"
+
+    exit_1
+  fi
+
+  # Remove 2 leading (tail -c +3) and 4 trailing (head -c -4)
+  # characters (inner-quoted parenthetical):
+  #   (' and ',)\n
+  # or
+  #   (" and ",)\n
+
+  local windows_list_blob
+  windows_list_blob="$(
+    printf '%s\n' "${windows_list_raw}" \
+      | head -c -4 \
+      | tail -c +3
+  )"
+
+  if [ -n "${RAISELOWER_TRACE_DIR}" ]; then
+    echo "${windows_list_blob}" \
+      > "${RAISELOWER_TRACE_DIR}/02--windows_list_blob--$(
+        ${is_double_quoted} && echo "double" || echo "single"
+      )"
+  fi
+
+  # ***
+
+  local windows_list_json
+  if ${is_double_quoted}; then
+    windows_list_json="$(
+      echo "${windows_list_blob}" \
+        | sed -e 's/\\\(\\\)\+"//g' \
+        | sed -e 's/\\"/"/g'
+    )"
+  elif ${is_single_quoted}; then
+    windows_list_json="$(
+      echo "${windows_list_blob}" \
+        | sed -e 's/\(\\\)\+"//g'
+    )"
+  fi
+
+  if [ -n "${RAISELOWER_TRACE_DIR}" ]; then
+    echo "${windows_list_json}" > "${RAISELOWER_TRACE_DIR}/03--windows_list_json"
+  fi
+
+  # ***
+
+  local window_ids
+  if ! window_ids="$(
+    echo "${windows_list_json}" \
+      | jq ".[] | ${jq_filter} ${jq_filter:+|} .id"
+  )"; then
+    # DEVEL: Run again with RAISELOWER_TRACE_DIR=. to debug.
+    >&2 echo "ERROR: Cannot determine windows ID(s) (from window-calls)"
+    if [ -n "${RAISELOWER_TRACE_DIR}" ]; then
+      echo -e "\njq_filter: ${jq_filter}" >> "${RAISELOWER_TRACE_DIR}/03--windows_list_json"
+    fi
+
+    return 1
+  fi
+
+  echo "${window_ids}"
+}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+# args: one or more window title(s) to try to match.
+raise_window_Wayland_titled() {
+  if [ $# -eq 0 ]; then
+    >&2 echo "ERROR: Please specify one or more window titles to match"
+
+    return 1
+
+  fi
+
+  # INERT/2025-09-20: Why not query window list for title,
+  # rather than getting IDs and probing each one separately?
+  # - DUNNO: Author doesn't remember why they wrote it this
+  #   way. But it works, so not much reason to futz with it.
+  local window_ids
+  if ! window_ids="$(get_window_ids_Wayland)"; then
+
+    return 1
+  fi
+
+  # ***
+
+  # Ignore errors, e.g.,
+  #   "Error: GDBus.Error:org.gnome.gjs.JSError.Error: Not found"
+  # though that probably only happens when you're debugging, and you're
+  # manually replaying windows_list you captured previously.
+
+  # ANFYI: If you later learn you need to process separate lines
+  # differently, perhaps you could pipe to a while loop, e.g.:
+  #
+  #   echo "${window_ids}" \
+  #     | xargs -I{} \
+  #         gdbus call --session --dest org.gnome.Shell \
+  #         --object-path /org/gnome/Shell/Extensions/Windows \
+  #         --method org.gnome.Shell.Extensions.Windows.Details \
+  #         {} 2> /dev/null \
+  #     | gawk 'match($0, /\{.*\}/, a) {print a[0]}' \
+  #     | while IFS= read -r line; do
+  #       printf "»%s«\n" "${line}"
+  #     done
+
+  # - Remove ('{ and }',) or ("{ and }",) from each line with gawk-match.
+  # - If outer container was ("{ and }",), the inner response is
+  #   escape-quoted (because single-quote(s) in window title) so
+  #   remove all double-or-more escaped quotes (those within a
+  #   window title); then convert escape-quotes to normal quotes.
+  # - USYNC: See similar pipeline in downstream app:
+  #   ~/.kit/sh/sh-humble-prompt/lib/show-command-name-in-window-title.sh
+  local window_details
+  window_details="$(
+    echo "${window_ids}" \
+      | xargs -I{} \
+        gdbus call --session --dest org.gnome.Shell \
+        --object-path /org/gnome/Shell/Extensions/Windows \
+        --method org.gnome.Shell.Extensions.Windows.Details \
+        {} 2> /dev/null \
+      | gawk 'match($0, /\{.*\}/, a) {print a[0]}' \
+      | sed -e 's/\\\(\\\)\+"//g' | sed -e 's/\\"/"/g'
+  )"
+
+  if [ -n "${RAISELOWER_TRACE_DIR}" ]; then
+    echo "${window_details}" > "${RAISELOWER_TRACE_DIR}/04--window_details"
+  fi
+
+  # ***
+
+  local title_and_ids
+  title_and_ids="$(
+    printf "%s" "${window_details}" \
+      | jq -r '"\(.title)\t\(.id)"' \
+        2>&1
+  )"
+
+  if [ $? -ne 0 ]; then
+    >&2 echo "ERROR: Failed to identify window:"
+    >&2 echo "  $ printf \"%s\" \"\${window_details}\" |"
+    >&2 echo "    jq -r '\"\\(.title)\\t\\(.id)\"'"
+    printf "%s" "${window_details}" \
+      | 1>&2 jq -r '"\(.title)\t\(.id)"'
+
+    return 1
+  fi
+
+  if [ -n "${RAISELOWER_TRACE_DIR}" ]; then
+    echo "${title_and_ids}" > "${RAISELOWER_TRACE_DIR}/05--title_and_ids"
+  fi
+
+  # ***
+
+  local window_id
+
+  local pattern
+  for pattern in "$@"; do
+    local title_and_id
+    # Use Perl regex, or \t doesn't work (though literal "${pattern}.*	"
+    # works, but not "${pattern}.*"$'\t', which is a Bashism anyway).
+    title_and_id="$(
+      echo "${title_and_ids}" | grep -P -e "${pattern}.*\t" | head -n1
+    )"
+
+    if [ -n "${title_and_id}" ]; then
+      window_id="$(echo "${title_and_id}" | sed 's/.*\t//')"
+
+      break
+    fi
+  done
+
+  if [ -n "${window_id}" ]; then
+    gdbus call --session --dest org.gnome.Shell \
+      --object-path /org/gnome/Shell/Extensions/Windows \
+      --method org.gnome.Shell.Extensions.Windows.Activate \
+      -- "${window_id}" > /dev/null
+
+    if [ -n "${RAISELOWER_TRACE_DIR}" ]; then
+      echo -e "\nwindow_id: ${window_id}" >> "${RAISELOWER_TRACE_DIR}/05--title_and_ids"
+    fi
+  else
+    >&2 echo "ALERT: No matching window identified for: $@"
+
+    return 1
+  fi
+}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+minimize_window_Wayland() {
+  local window_id="$1"
+
+  # OUTPUTs: ()
+  gdbus call --session --dest org.gnome.Shell \
+    --object-path /org/gnome/Shell/Extensions/Windows \
+    --method org.gnome.Shell.Extensions.Windows.Minimize \
+    -- "${window_id}" > /dev/null
+}
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ #
+
+check_deps_gnome_extension_window_calls() {
+  # Check that the extension path exists.
+  # - This might be excessive; the dconf-read might be sufficient
+  #   (and less fragile than checking the bespoke path here).
+  local uuid="window-calls@domandoman.xyz"
+  if ! [ -d "${XDG_DATA_HOME:-${HOME}/.local/share}/gnome-shell/extensions/${uuid}" ]; then
+    alert_missing_gnome_extension_window_calls
+
+    return 1
+  fi
+
+  local arr_index=""
+  arr_index=$(
+    dconf read /org/gnome/shell/enabled-extensions \
+      | sed "s/'/\"/g" \
+      | jq -r ". | index(\"${uuid}\")"
+  )
+
+  if [ "${arr_index}" = "null" ]; then
+
+    return 1
+  fi
+}
+
+alert_missing_gnome_extension_window_calls() {
+  >&2 echo "ERROR: Missing GNOME Extension: window-calls"
+  >&2 echo "  https://extensions.gnome.org/extension/4724/window-calls/"
+  >&2 echo "  https://github.com/ickyicky/window-calls"
+}
+
+# ***
--- deps/sh-humble-prompt/lib/show-command-name-in-window-title.sh
+++ deps/sh-humble-prompt/lib/show-command-name-in-window-title.sh
@@ -254,17 +254,24 @@ _humb_print_terminal_window_number_linux_terminal() {
 
 _humb_print_terminal_window_title_prefixes() {
   if [ "$(_humb_probe_desktop_environment)" = "GNOME" ]; then
-    _humb_print_terminal_window_title_prefixes_Wayland
+    # Run in subshell to avoid polluting env.
+    # w/ third-party deps/ funcs.
+    (_humb_print_terminal_window_title_prefixes_Wayland)
   else
     _humb_print_terminal_window_title_prefixes_XWindow
   fi
 }
 
+# ***
+
 # SAVVY: You can use wmctrl in Wayland to some extent, e.g.:
 #   $ sudo apt install wmctrl && wmctrl -m
 #   Name: GNOME Shell
-# But `wmctrl -l` shows a very limited subset of windows,
-# e.g., author only sees Chrome and GVim windows listed.
+# But `wmctrl -l` won't show all windows, e.g., author only
+# sees Chrome and GVim windows listed.
+# - So for GNOME, we'll use a special (and optional) GNOME
+#   Shell extension that exposes a D-Bus interface to work
+#   with windows on Wayland.
 
 _humb_probe_desktop_environment() {
   # Colon-separated list, uppercased.
@@ -288,61 +295,80 @@ _humb_probe_desktop_environment() {
   )
 }
 
-# USAGE: Requires GNOME Shell Extension:
+# ***
+
+# REFER: Uses integrated dependency to query Wayland window titles.
+#   https://github.com/DepoXy/gnome-window-calls#🪟
+# ~/.kit/sh/gnome-window-calls/lib/gnome-window-calls.sh
+
+# DPNDS: Requires `window-calls` GNOME Shell extension:
 #   https://extensions.gnome.org/extension/4724/window-calls/
 #   https://github.com/ickyicky/window-calls
-# REFER:
-# ~/.local/share/gnome-shell/extensions/
+# REFER: Installs to:
+#   ~/.local/share/gnome-shell/extensions/window-calls@domandoman.xyz/
+
+# SAVVY:
+# - Use jq filter to pick gnome-terminal window IDs only.
+# - For each ID, send gdbus command to window-calls
+#   extension to get window details, including title.
+# - Use gawk to remove the '(' ... ',)' around details.
+# - Remove embedded, escaped double quotes;
+#   and resolve JSON string escapes.
+#   - See raise-lower for comments — the Windows.Details
+#     response varies based on `'` and/or `"` characters
+#     in a window's title.
+#   - Basically, if any double-quote is double-or-more
+#     escaped, remove it (because it's part of a JSON
+#     string); and then convert all \" to " (because
+#     the outer window-call response was double-quoted,
+#     and the embedded JSON string used escape-quoting
+#     for JSON fields and values).
+# - Use jq to print each window title.
+# - Use awk to print only the first column, e.g.,
+#   '1.', '2.', etc.
 
 _humb_print_terminal_window_title_prefixes_Wayland() {
-  local windows_list
-  if ! windows_list="$(
-    gdbus call --session --dest org.gnome.Shell \
-      --object-path /org/gnome/Shell/Extensions/Windows \
-      --method org.gnome.Shell.Extensions.Windows.List
-  )"; then
-    # E.g. — Error: GDBus.Error:org.freedesktop.DBus.Error.UnknownMethod:
-    #   Object does not exist at path “/org/gnome/Shell/Extensions/Foo”
+  # Load: get_window_ids_Wayland_filtered
+  # - REFER: This dependency is included with the project,
+  #   under the deps/ directory.
+  #   - CXREF: In a DepoXy env., you'll find the original at:
+  #     ~/.kit/sh/gnome-window-calls/lib/gnome-window-calls.sh
+  local canon_base
+  canon_base="$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")"
+  . "${canon_base}/../deps/gnome-window-calls/lib/gnome-window-calls.sh"
+
+  local jq_filter='select(
+    .wm_class == "gnome-terminal-server" or
+    .wm_class == "Alacritty"
+  )'
+
+  local window_ids
+  if ! window_ids="$(
+    get_window_ids_Wayland_filtered "${jq_filter}"
+  )" 2>/dev/null; then
+    # Emits error (that we inhibited) if window-calls not installed.
+    # - We don't print error. User should notice if the number prefix
+    #   is missing, and then they can investigate themselves, e.g.,
+    #   run `get_window_ids_Wayland_filtered` manually.
 
     return 1
   fi
 
-  # SAVVY:
-  # - Use jq to pick gnome-terminal windows only,
-  #   and emit a list of IDs.
-  # - For each ID, send gdbus command to window-calls
-  #   Extension to get window details, including title.
-  # - Use gawk to remove the '(' ... ',)' around details.
-  # - Remove double quote delimiter, for jq.
-  # - Use jq to print each window title.
-  # - Use awk to print only the first column, e.g.,
-  #   '1.', '2.', etc.
-
-  # REFER: See comments in DepoXy re: How to deal with escapes:
-  #   https://github.com/DepoXy/depoxy#🍯
-  #     ~/.depoxy/ambers/bin/windows/toggle-visibility
-  # - Removing escape characters before quotes usually works:
-  #     | sed 's/\\"/"/g' \
-  #   But sometimes the response is doubly-delimited, which we
-  #   need to transform for `jq` not to fail on the first line
-  #   that's doubly-escaped (\\"):
-  #     | sed -e 's/\\"/"/g' -e 's/\\\\"/\\"/g' \
-
-  echo "${windows_list}" | head -c -4 | tail -c +3 |
-    jq '.[] | select(
-      .wm_class == "gnome-terminal-server"
-      or .wm_class == "Alacritty"
-    ) | .id' |
+  # USYNC: See similar pipeline in upstream lib:
+  # ~/.kit/sh/gnome-window-calls/lib/gnome-window-calls.sh
+  echo "${window_ids}" |
     xargs -I{} \
       gdbus call --session --dest org.gnome.Shell \
       --object-path /org/gnome/Shell/Extensions/Windows \
       --method org.gnome.Shell.Extensions.Windows.Details \
       {} |
     gawk 'match($0, /\{.*\}/, a) {print a[0]}' |
-    sed -e 's/\\"/"/g' -e 's/\\\\"/\\"/g' |
+    sed -e 's/\\\(\\\)\+"//g' | sed -e 's/\\"/"/g' |
     jq -r '.title' |
     awk '{print $1}'
 }
+
+# ***
 
 # CALSO:
 #   xdotool search --onlyvisible -class mate-terminal getwindowname %@
@@ -375,6 +401,12 @@ _humb_cleanup_lib_term_window_title_show_command_name() {
   # Leave set: ITERM2_WINDOW_NUMBER
 
   unset -f _humb_titler_os_is_macos
+
+  unset -f _humb_probe_desktop_environment
+
+  unset -f _humb_print_terminal_window_title_prefixes
+  unset -f _humb_print_terminal_window_title_prefixes_Wayland
+  unset -f _humb_print_terminal_window_title_prefixes_XWindow
 
   unset -f _humb_print_terminal_window_number
   unset -f _humb_print_terminal_window_number_iterm
