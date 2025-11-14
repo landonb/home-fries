aa8ede908f02d30f2c3aa24f51072c13e746bb20
Landon Bouma <landonb@users.noreply.github.com>
2025-11-13 18:02:43 -0600

    deps: update faithfuls <sh-humble-prompt>
    - Source: sh-humble-prompt @ 376badc50399 [1.2.0 at HEAD~1 “86e4711”]
    
    - Commit generated by:
    
        https://github.com/thegittinsgood/git-update-faithful#⛲
    
      under the guidance of:
    
        https://github.com/DepoXy/depoxy/blob/release/home/.kit/git/ohmyrepos/lib/my-deps-manage-shoilerplate.sh

--- deps/sh-humble-prompt/lib/set-shell-prompt-and-window-title.sh
+++ deps/sh-humble-prompt/lib/set-shell-prompt-and-window-title.sh
@@ -228,8 +228,44 @@ _humb_prompt_customize_shell_prompts_and_window_title() {
   #      34    Blue   35 Magenta  36 Cyan   37 White
   #   X=3 is Foreground, =4 is Background colors, i.e., 47 is White BG
   # export PS1='\[\033[1;37m\]\u@\[\033[1;33m\]\h\[\033[1;36m\][\W\[\033[00m\]]: '
-  # # From debian .bashrc:
-  # PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+  #
+  # REFER/2025-11-11: From Debian 13 stock ~/.bashrc, which uses value in its PS1:
+  #   # set variable identifying the chroot you work in (used in the prompt below)
+  #   if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+  #     debian_chroot=$(cat /etc/debian_chroot)
+  #   fi
+  #   # set a fancy prompt (non-color, unless we know we "want" color)
+  #   case "$TERM" in
+  #     xterm-color|*-256color) color_prompt=yes;;
+  #   esac
+  #   # uncomment for a colored prompt, if the terminal has the capability; turned
+  #   # off by default to not distract the user: the focus in a terminal window
+  #   # should be on the output of commands, not on the prompt
+  #   #force_color_prompt=yes
+  #   if [ -n "$force_color_prompt" ]; then
+  #     if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+  #       # We have color support; assume it's compliant with Ecma-48
+  #       # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+  #       # a case would tend to support setf rather than setaf.)
+  #       color_prompt=yes
+  #     else
+  #       color_prompt=
+  #     fi
+  #   fi
+  #   if [ "$color_prompt" = yes ]; then
+  #     PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+  #   else
+  #     PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+  #   fi
+  #   unset color_prompt force_color_prompt
+  #   # If this is an xterm set the title to user@host:dir
+  #   case "$TERM" in
+  #   xterm*|rxvt*)
+  #     PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+  #     ;;
+  #   *)
+  #     ;;
+  #   esac
 
   local titlebar="$(_humb_prompt_format_titlebar)"
 
