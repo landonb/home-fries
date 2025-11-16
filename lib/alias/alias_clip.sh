cbd01cbccf13fe6f62e8bdbf92cca3e7b454294c
Landon Bouma <landonb@users.noreply.github.com>
2025-11-15 22:25:47 -0600

    fix: `clear` hangs over SSH to different host than desktop
    - The wl-copy command hangs when run over SSH, unless SSH'd
      to the same host as the desktop manager (even if you SSH
      to a remote host, and then SSH back to your original host).
    
      - This is a known issue:
    
        https://github.com/bugaevc/wl-clipboard/pull/154
    
    - I also fixed some syntax errors in the elif conditionals.
    
      - Specifically, unquoted -n tests are always true! e.g.,
    
          [ -n ${undefined_variable} ]
    
        is always true.
    
        - (Oof! How embarrassing)
    
                (,,>﹏<,,)
    
      - So the wl-copy elif's were always running on GNOME Shell,
        regardless of $WAYLAND_DISPLAY being declared or not.
    
      - But now this is fixed, and I've added an additional elif
        to check if the SSH session is connected to the originating
        desktop host or not, so we can avoid hanging because of
        the wl-copy issue referenced above.
    
    - Finally, the clipboard copy pipeline will now warn-tell if the
      clipboard is inaccessible... Although I may revert this change
      soon. But I'd like to demo it for now. (Homefries defines some
      helpful copy pipeline commands like `pp` to print and copy the
      `pwd`; and DepoXy also defines some helpful copy pipeline cmds,
      like `fdp` to print and copy the result from the `fd` cmd, and
      `stp` to print/copy output from the `git status` command, etc.
      And while I don't think I normally run these commands over SSH,
      I'm not certain, so I don't expect to see the new warn message
      very often. Or maybe I'm wrong & I will; I guess I'll FA & FO.)

--- lib/alias/alias_clip.sh
+++ lib/alias/alias_clip.sh
@@ -23,12 +23,46 @@ _hf_clip() {
 _hf_clip_raw() {
   if os_is_macos && command -v pbcopy >/dev/null; then
     pbcopy
-  elif [ -n ${WAYLAND_DISPLAY} ] && command -v wl-copy >/dev/null; then
+  elif [ -n "${WAYLAND_DISPLAY}" ] && command -v wl-copy >/dev/null; then
     # CALSO: wl-copy --trim-newline
     wl-copy
-  elif [ -n ${DISPLAY} ] && command -v xclip >/dev/null; then
+  elif [ -n "${DISPLAY}" ] && command -v xclip >/dev/null; then
     # Aka: xclip -selection clip
     xclip -selection c
+  elif command -v wl-copy >/dev/null; then
+    # E.g.,
+    #   $ host ${SSH_CONNECTION%% *}
+    #   123.1.168.192.in-addr.arpa domain name pointer host.lan.
+    # - THANX:
+    #   https://askubuntu.com/questions/421033/
+    #     determining-the-name-of-the-host-currently-connected-via-ssh
+    # INERT/FTREQ: Pass originating host as SSH variable, so you can
+    # detect SSH from host1 → host2 → host1, because wl-copy works
+    # on host1, even though connected through intermediate remote.
+    # - Though not a very likely use case, but we could support it.
+    # - For now, this check won't detect that case; it will only
+    #   detected SSH from host1 → host1.
+    local connected_from
+    connected_from="$(
+      host ${SSH_CONNECTION%% *} | sed 's/^.* domain name pointer \(.*\)\.lan\.$/\1/'
+    )"
+    # BWARE: wl-copy hangs when called over SSH from separate host.
+    # - When SSH'd to same machine as desktop host, wl-copy works (you
+    #   can copy to or clear it).
+    # - REFER: But when SSH'd to a difference machine from the desktop
+    #   host, wl-copy hangs (until interrupted), which is a known issue:
+    #     https://github.com/bugaevc/wl-clipboard/pull/154
+    #     - BEGET: https://www.google.com/search?q=wl-copy+hangs+over+ssh
+    #   - One suggestion is using 2>/dev/null, but that doesn't work.
+    if [ "${connected_from}" = "$(hostname)" ]; then
+      wl-copy
+    else
+      # else, over SSH, so do nothing, you're not in control of that
+      # host's clipboard.
+      # - ALTLY: We could tell user what they're trying to do is not
+      #   supported... but, uh... should we?
+      >&2 echo "ERROR: Cannot access clipboard on this host"
+    fi
   fi
 }
 
