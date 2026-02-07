676187f1c9bad4602f78a18aa8fe53bde057b2bb
Landon Bouma <landonb@users.noreply.github.com>
2026-02-06 20:37:43 -0600

    improve(du): format `dum`, `dub` output so columns align

--- lib/alias/alias_du.sh
+++ lib/alias/alias_du.sh
@@ -9,9 +9,9 @@
 home_fries_aliases_wire_du() {
   alias du="du -h"
 
-  claim_alias_or_warn "dum" "du -m -d 1 . | sort -n"
+  claim_alias_or_warn "dum" "du -m -d 1 . | sort -n | column -t"
 
-  claim_alias_or_warn "dub" "du -b -d 1 . | sort -n"
+  claim_alias_or_warn "dub" "du -b -d 1 . | sort -n | column -t"
 
   # 2024-06-13: Duh.
   claim_alias_or_warn "duh" "du -h -d 1 ."
