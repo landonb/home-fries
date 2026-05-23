1b22827892e623686c5e1c1a7b66905afe5fad6a
Landon Bouma <landonb@users.noreply.github.com>
2026-05-23 12:58:48 -0500

    feat!(cmds(pwgen)): rename environ: PWGEN_LENGTH → PWGEN_PINLEN

--- lib/alias/alias_pwgen.sh
+++ lib/alias/alias_pwgen.sh
@@ -34,7 +34,7 @@ home_fries_aliases_wire_pwgen() {
   claim_alias_or_warn "pwgen23" "_hf_aliases_wire_pwgen_clip_and_print"
 
   claim_alias_or_warn "pwgenpin" \
-    "pwgen -A -r abcdefghijklmnopqrstuvwxyz ${PWGEN_LENGTH:-4} \
+    "pwgen -A -r abcdefghijklmnopqrstuvwxyz ${PWGEN_PINLEN:-4} \
     | _hf_clip_echo"
 }
 
